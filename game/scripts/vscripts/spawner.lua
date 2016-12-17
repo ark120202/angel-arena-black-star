62f85d7e59849fc7ee34d1205adc958ba1fa11e8
Alastor029711 <vba98765@mail.ru>
2016-12-17 20:56:21 +0300

    Нерф героев

--- game/scripts/vscripts/spawner.lua
+++ game/scripts/vscripts/spawner.lua
@@ -88,13 +88,13 @@ function Spawner:UpgradeCreep(unit, spawnerType, minutelevel, spawnerIndex)
 	end
 	if spawnerType == "easy" then
 		if minutelevel <= 10 then
-			goldbounty = 2 * minutelevel
-			hp = 10 * minutelevel
-			damage = 2.5 * minutelevel
+			goldbounty = 1.9 * minutelevel
+			hp = 12 * minutelevel
+			damage = 2.8 * minutelevel
 			attackspeed = 0.30 * minutelevel
 			movespeed = 0.30 * minutelevel
 			armor = 0.30 * minutelevel
-			xpbounty = 8 * minutelevel
+			xpbounty = 10 * minutelevel
 		elseif minutelevel <= 20 then
 			goldbounty = 3 * minutelevel
 			hp = 20 * minutelevel
@@ -131,13 +131,13 @@ function Spawner:UpgradeCreep(unit, spawnerType, minutelevel, spawnerIndex)
 	end
 	if spawnerType == "medium" then
 		if minutelevel <= 10 then
-			goldbounty = 3 * minutelevel
-			hp = 15 * minutelevel
-			damage = 2.5 * minutelevel
+			goldbounty = 2.9 * minutelevel
+			hp = 18 * minutelevel
+			damage = 2.8 * minutelevel
 			attackspeed = 0.70 * minutelevel
 			movespeed = 0.70 * minutelevel
 			armor = 0.70 * minutelevel
-			xpbounty = 6 * minutelevel
+			xpbounty =4 * minutelevel
 		elseif minutelevel <= 20 then
 			goldbounty = 17 * minutelevel
 			hp = 110 * minutelevel
@@ -145,7 +145,7 @@ function Spawner:UpgradeCreep(unit, spawnerType, minutelevel, spawnerIndex)
 			attackspeed = 0.75 * minutelevel
 			movespeed =0.75  * minutelevel
 			armor = 0.75 * minutelevel
-			xpbounty = 30 * minutelevel
+			xpbounty = 25 * minutelevel
 		elseif minutelevel <= 30 then
 			goldbounty = 30 * minutelevel
 			hp = 240 * minutelevel
@@ -174,13 +174,13 @@ function Spawner:UpgradeCreep(unit, spawnerType, minutelevel, spawnerIndex)
 	end
 	if spawnerType == "hard" then
 		if minutelevel <= 10 then
-			goldbounty = 3 * minutelevel
+			goldbounty = 2.9 * minutelevel
 			hp = 15 * minutelevel
-			damage = 10 * minutelevel
+			damage = 8 * minutelevel
 			attackspeed = 1 * minutelevel
 			movespeed = 1 * minutelevel
 			armor = 1 * minutelevel
-			xpbounty = 8 * minutelevel
+			xpbounty = 5 * minutelevel
 		elseif minutelevel <= 20 then
 			goldbounty = 15 * minutelevel
 			hp = 100 * minutelevel
