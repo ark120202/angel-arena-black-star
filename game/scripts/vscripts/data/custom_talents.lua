5ad22df6704174ce1b600023f6063cbac077e1e3
Alastor029711 <vba98765@mail.ru>
2017-01-03 00:52:03 +0300

    Таланты

--- game/scripts/vscripts/data/custom_talents.lua
+++ game/scripts/vscripts/data/custom_talents.lua
@@ -1,34 +1,134 @@
 CUSTOM_TALENTS_DATA = {
-	talent_wtf = {
-		icon = "wtf", -- /custom_game/talents/<icon>.png
-		cost = 1,
+	talent_Exp = {
+		icon = "wtf",
+		cost = 4,
 		group = 1,
-		--requirement = "npc_dota_hero_abaddon", -- hero or ability name
-		max_level = 6,
+		max_level = 7,
 		special_values = {
-			["damage"] = {111,222,333,444,555,666}
+			["ДОП ОПЫТ"] = {10, 15, 20, 25, 30, 35, 40,}
 		},
 		effect = {
 			modifiers = {
-				["modifier_talent_damage"] = "damage",
+				["modifier_talent_damage"] = "доп опыт",
 			},
 		}
 	},
-	talent_hoooks = {
-		icon = "wtf", -- /custom_game/talents/<icon>.png
-		cost = 156, --ability points cost
-		group = 4, -- level group
-		requirement = "pudge_meat_hook_lua", -- hero or ability name
+
+	talent_Gold = {
+		icon = "wtf",
+		cost = 8,
+		group = 1,
+		max_level = 7,
 		special_values = {
-			["hooks"] = 16
+			["Доп золото"] = {5, 8, 12, 14, 16, 18, 20,}
+		},
+		effect = {
+			modifiers = {
+				["modifier_talent_damage"] = "Доп золото",
+			},
 		}
 	},
+
+	talent_HUD = {
+		icon = "wtf",
+		cost = 3,
+		group = 2,
+		max_level = 7,
+		special_values = {
+			["Dop HUD"] = {3, 6, 9, 12, 15, 18, 21,}
+		},
+		effect = {
+			modifiers = {
+				["modifier_talent_damage"] = "Dop HUD",
+			},
+		}
+	},
+
+	talent_damage_magical = {
+		icon = "wtf",
+		cost = 5,
+		group = 2,
+		max_level = 7,
+		special_values = {
+			["Damage_Magical"] = {10, 20, 30, 40, 50, 60, 70,}
+		},
+		effect = {
+			modifiers = {
+				["modifier_talent_damage"] = "Damage_Magical",
+			},
+		}
+	},
+
+	talent_respawn = {
+		icon = "wtf",
+		cost = 12,
+		group = 10,
+		max_level = 7,
+		special_values = {
+			["Respawn"] = { -10, -15, -20, -25, -30, -35, -40,}
+		},
+		effect = {
+			modifiers = {
+				["modifier_talent_damage"] = "Respawn",
+			},
+		}
+	},
+
+	talent_damage_physical = {
+		icon = "wtf",
+		cost = 2,
+		group = 4,
+		max_level = 7,
+		special_values = {
+			["damage_physical"] = {200, 400, 600, 800, 1000, 1200, 1400,}
+		},
+		effect = {
+			modifiers = {
+				["modifier_talent_damage"] = "damage_physical",
+			},
+		}
+	},
+
+	talent_Evasion = {
+		icon = "wtf",
+		cost = 4,
+		group = 3,
+		max_level = 7,
+		special_values = {
+			["evasion"] = {5, 10, 15, 20, 25, 30, 35,}
+		},
+		effect = {
+			modifiers = {
+				["modifier_talent_damage"] = "evasion",
+			},
+		}
+	},
+
 }
 
 TALENT_GROUP_TO_LEVEL = {
-	[1] = 0,
-	[2] = 4,
-	[3] = 8,
-	[4] = 12,
-	[5] = 16,
+	[1] = 10,
+	[2] = 30,
+	[3] = 50,
+	[4] = 70,
+	[5] = 90,
+	[6] = 120,
+	[7] = 140,
+	[8] = 160,
+	[10] = 180,
+	[11] = 200,
+	[12] = 220,
+	[14] = 240,
+	[15] = 260,
+	[16] = 280,
+	[17] = 300,
+	[18] = 320,
+	[19] = 340,
+	[20] = 360,
+	[21] = 380,
+	[22] = 400,
+	[23] = 430,
+	[24] = 460,
+	[25] = 490,
+	[26] = 550,
 }
