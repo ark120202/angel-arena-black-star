50c736327faad982195f8830f9ee2bf0457c0008
ark120202 <ark120202@gmail.com>
2017-06-09 08:33:51 +0500

    Fixed server url

--- game/scripts/vscripts/modules/stats/data.lua
+++ game/scripts/vscripts/modules/stats/data.lua
@@ -1,3 +1,3 @@
 StatsClient.Debug = false
-StatsClient.ServerAddress = IsInToolsMode() and "https://stats.dota-aabs.com/" or "http://127.0.0.1:6502/"
+StatsClient.ServerAddress = not IsInToolsMode() and "https://stats.dota-aabs.com/" or "http://127.0.0.1:6502/"
 StatsClient.RetryDelay = 5
