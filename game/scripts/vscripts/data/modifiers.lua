f0356fd85f8f6e984f2cecbd3b088f47a7791ccc
ark120202 <ark120202@gmail.com>
2017-06-11 05:30:54 +0500

    Black King Bar's and Titanium Bar's active modifiers now will be removed on duel

--- game/scripts/vscripts/data/modifiers.lua
+++ game/scripts/vscripts/data/modifiers.lua
@@ -23,6 +23,8 @@ DUEL_PURGED_MODIFIERS = {
 	"modifier_tether_ally_aghanims",
 	"modifier_life_stealer_assimilate",
 	"modifier_life_stealer_assimilate_effect",
+	"modifier_item_black_king_bar_arena_active",
+	"modifier_item_titanium_bar_active"
 }
 
 ABILITY_INVULNERABLE_UNITS = {
