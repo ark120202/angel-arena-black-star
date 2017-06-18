6b749a591622a968804216b493fec73cbb3dc6f5
ark120202 <ark120202@gmail.com>
2017-06-18 18:14:13 +0500

    Fixed Phoenix - Sun Ray + Fountain server crash

--- game/scripts/vscripts/triggers.lua
+++ game/scripts/vscripts/triggers.lua
@@ -20,13 +20,20 @@ function ArenaZoneOnEndTouch(trigger)
 	end
 end
 
+local OUT_OF_GAME_UNITS = {
+	[""] = true,
+	npc_dota_thinker = true,
+	npc_dummy_unit = true,
+	npc_dota_looping_sound = true,
+	npc_dota_invisible_vision_source = true,
+}
 function FountainOnStartTouch(trigger, team)
 	local unit = trigger.activator
 	if unit and unit:GetTeam() == team then
 		unit:AddNewModifier(unit, nil, "modifier_fountain_aura_arena", nil)
 	elseif not unit:IsBoss() then
 		local unitName = unit:GetUnitName()
-		if unitName ~= "" and unitName ~= "npc_dota_thinker" and unitName ~= "npc_dummy_unit" then
+		if not OUT_OF_GAME_UNITS[unitName] then
 			if GameRules:GetDOTATime(false, true) < FOUNTAIN_EFFECTIVE_TIME_THRESHOLD then
 				Timers:CreateTimer(0.1, function()
 					local fountain = FindFountain(team)
