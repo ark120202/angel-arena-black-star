99eac9370ee503e799858eab1a566066ee7eb195
XELLA\Александр <fackyouprodaction@gmail.com>
2017-07-01 17:13:10 +0300

    -"Dark Blink" uses 0.5% of maximum energy for every 50 range now. If range less, than standart 600 range, this rule dont work. -"Dark Blink" have manacost now - 5% from max energy. -Added localization for new mechanic of "Dark Blink".

--- game/scripts/vscripts/heroes/hero_sara/dark_blink.lua
+++ game/scripts/vscripts/heroes/hero_sara/dark_blink.lua
@@ -1,13 +1,18 @@
 sara_dark_blink = class({})
 
+if IsClient() then
+	function sara_dark_blink:GetManaCost()
+		return self:GetCaster():GetMaxMana() * self:GetSpecialValueFor("min_cost_pct") * 0.01
+	end
+end
+
 if IsServer() then
 	function sara_dark_blink:OnSpellStart()
 		local caster = self:GetCaster()
 		if caster.GetEnergy then
 			local energyBeforeCast = caster:GetEnergy()
-			local wasted = math.max(self:GetSpecialValueFor("min_cost"), caster:GetEnergy() * self:GetSpecialValueFor("energy_pct") * 0.01)
+			local wasted = math.round(self:GetCaster():GetMaxMana() * self:GetSpecialValueFor("min_cost_pct") * 0.01)
 			if caster:GetEnergy() >= wasted then
-				caster:ModifyEnergy(-wasted)
 
 				local point = self:GetCursorPosition()
 				local casterPos = caster:GetAbsOrigin()
@@ -16,6 +21,13 @@ if IsServer() then
 				if (point - casterPos):Length2D() > blinkRange then
 					point = casterPos + (point - casterPos):Normalized() * blinkRange
 				end
+
+				local cost = math.round((point - casterPos):Length2D() / self:GetSpecialValueFor("range_per_to_cost") * self:GetSpecialValueFor("energy_to_range_per_pct") * caster:GetMaxEnergy() * 0.01)
+				if (point - casterPos):Length2D() >= self:GetSpecialValueFor("blink_range") then
+					caster:ModifyEnergy(-cost)
+				end
+				caster:ModifyEnergy(-wasted)
+
 				caster:EmitSound('Hero_Antimage.Blink_out')
 				ParticleManager:CreateParticle("particles/arena/units/heroes/hero_sara/dark_blink_start.vpcf", PATTACH_ABSORIGIN, caster)
 				FindClearSpaceForUnit(caster, point, false)
