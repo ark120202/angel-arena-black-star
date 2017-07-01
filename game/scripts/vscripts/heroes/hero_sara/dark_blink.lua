9d8d5de8bb53155fc6e4b5441765a2aef398b799
ark120202 <ark120202@gmail.com>
2017-07-01 21:05:20 +0500

    Lots of Sara's description updates and small fixes to Dark Blink logic

--- game/scripts/vscripts/heroes/hero_sara/dark_blink.lua
+++ game/scripts/vscripts/heroes/hero_sara/dark_blink.lua
@@ -11,8 +11,10 @@ if IsServer() then
 		local caster = self:GetCaster()
 		if caster.GetEnergy then
 			local energyBeforeCast = caster:GetEnergy()
-			local wasted = math.round(self:GetCaster():GetMaxMana() * self:GetSpecialValueFor("min_cost_pct") * 0.01)
+			local maxEnergy = caster:GetMaxEnergy()
+			local wasted = math.round(maxEnergy * self:GetSpecialValueFor("min_cost_pct") * 0.01)
 			if caster:GetEnergy() >= wasted then
+				caster:ModifyEnergy(-wasted)
 
 				local point = self:GetCursorPosition()
 				local casterPos = caster:GetAbsOrigin()
@@ -22,11 +24,11 @@ if IsServer() then
 					point = casterPos + (point - casterPos):Normalized() * blinkRange
 				end
 
-				local cost = math.round((point - casterPos):Length2D() / self:GetSpecialValueFor("range_per_to_cost") * self:GetSpecialValueFor("energy_to_range_per_pct") * caster:GetMaxEnergy() * 0.01)
-				if (point - casterPos):Length2D() >= self:GetSpecialValueFor("blink_range") then
-					caster:ModifyEnergy(-cost)
+				local additionalCostSteps = math.ceil(((point - casterPos):Length2D() - self:GetSpecialValueFor("blink_range")) / self:GetSpecialValueFor("range_energy_step"))
+				local additionalCost = additionalCostSteps * maxEnergy * self:GetSpecialValueFor("max_energy_per_step_pct") * 0.01
+				if additionalCost > 0 then
+					caster:ModifyEnergy(-additionalCost)
 				end
-				caster:ModifyEnergy(-wasted)
 
 				caster:EmitSound('Hero_Antimage.Blink_out')
 				ParticleManager:CreateParticle("particles/arena/units/heroes/hero_sara/dark_blink_start.vpcf", PATTACH_ABSORIGIN, caster)
