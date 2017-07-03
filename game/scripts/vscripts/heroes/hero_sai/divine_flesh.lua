c610d23a19a5f346c37b1a82257636fe80e76f97
XELLA\Александр <fackyouprodaction@gmail.com>
2017-07-03 19:42:31 +0300

    -Sai's skills located in one files now

--- game/scripts/vscripts/heroes/hero_sai/divine_flesh.lua
+++ game/scripts/vscripts/heroes/hero_sai/divine_flesh.lua
@@ -1,6 +1,6 @@
 sai_divine_flesh = class({})
-LinkLuaModifier("modifier_sai_divine_flesh_on", "heroes/hero_sai/modifier_sai_divine_flesh.lua", LUA_MODIFIER_MOTION_NONE)
-LinkLuaModifier("modifier_sai_divine_flesh_off", "heroes/hero_sai/modifier_sai_divine_flesh.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_sai_divine_flesh_on", "heroes/hero_sai/divine_flesh.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_sai_divine_flesh_off", "heroes/hero_sai/divine_flesh.lua", LUA_MODIFIER_MOTION_NONE)
 
 function sai_divine_flesh:GetIntrinsicModifierName()
 	return "modifier_divine_flesh_off"
@@ -16,5 +16,85 @@ if IsServer() then
 			caster:RemoveModifierByName("modifier_sai_divine_flesh_on")
 			caster:AddNewModifier(caster, self, "modifier_sai_divine_flesh_off", nil)
 		end
+	end
+end
+
+modifier_sai_divine_flesh_on = class({})
+function modifier_sai_divine_flesh_on:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
+		MODIFIER_PROPERTY_MAGICAL_RESISTANCE_BONUS
+	}
+end
+function modifier_sai_divine_flesh_on:GetModifierPhysicalArmorBonus()
+	local ability = self:GetAbility()
+	return ability:GetSpecialValueFor("active_bonus_armor")
+end
+function modifier_sai_divine_flesh_on:GetModifierMagicalResistanceBonus()
+	local ability = self:GetAbility()
+	return ability:GetSpecialValueFor("active_magic_resistance_pct")
+end
+
+modifier_sai_divine_flesh_off = class({})
+function modifier_sai_divine_flesh_off:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_HEALTH_REGEN_PERCENTAGE
+	}
+end
+function modifier_sai_divine_flesh_off:GetModifierHealthRegenPercentage()
+	local ability = self:GetAbility()
+	local unit = self:GetParent()
+	if unit:FindAbilityByName("sai_immortal"):GetToggleState()==true and
+		self:GetParent():GetMana() >= unit:FindAbilityByName("sai_immortal"):GetSpecialValueFor("mana_per_second") then
+		return ability:GetSpecialValueFor("health_regeneration_pct") * 
+		unit:FindAbilityByName("sai_immortal"):GetSpecialValueFor("bonus_regen")
+	else
+		return ability:GetSpecialValueFor("health_regeneration_pct")
+	end
+end
+
+function modifier_sai_divine_flesh_on:IsHidden()
+	return true
+end
+function modifier_sai_divine_flesh_off:IsHidden()
+	return true
+end
+
+if IsServer() then
+	function modifier_sai_divine_flesh_on:OnCreated()
+		self:StartIntervalThink(self:GetAbility():GetSpecialValueFor("think_interval"))
+		self:OnIntervalThink()
+	end
+end
+
+function modifier_sai_divine_flesh_on:OnIntervalThink()
+	local ability = self:GetAbility()
+	local parent = self:GetParent()
+	local unit = parent
+	local damage = parent:GetMaxHealth()/100*ability:GetSpecialValueFor("active_self_damage_pct") *
+			ability:GetSpecialValueFor("think_interval")
+
+	if unit:FindAbilityByName("sai_immortal"):GetToggleState()==true and
+	self:GetParent():GetMana() >= unit:FindAbilityByName("sai_immortal"):GetSpecialValueFor("mana_second") then
+		damage = damage/100*unit:FindAbilityByName("sai_immortal"):GetSpecialValueFor("damage_reduction_pct")
+
+		ApplyDamage({
+		victim = parent,
+		attacker = parent,
+		damage = damage,
+		damage_type = DAMAGE_TYPE_PURE,
+		damage_flags = DOTA_DAMAGE_FLAG_NO_DAMAGE_MULTIPLIERS + DOTA_DAMAGE_FLAG_HPLOSS,
+		ability = ability
+	})
+	else
+
+		ApplyDamage({
+		victim = parent,
+		attacker = parent,
+		damage = damage,
+		damage_type = DAMAGE_TYPE_PURE,
+		damage_flags = DOTA_DAMAGE_FLAG_NO_DAMAGE_MULTIPLIERS + DOTA_DAMAGE_FLAG_HPLOSS,
+		ability = ability
+	})
 	end
 end
--- game/scripts/vscripts/heroes/hero_sai/immortal.lua
+++ game/scripts/vscripts/heroes/hero_sai/immortal.lua
@@ -1,5 +1,5 @@
 sai_immortal = class({})
-LinkLuaModifier("modifier_sai_immortal", "heroes/hero_sai/modifier_sai_immortal.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_sai_immortal", "heroes/hero_sai/immortal.lua", LUA_MODIFIER_MOTION_NONE)
 
 if IsServer() then
 	function sai_immortal:OnToggle()
@@ -9,5 +9,62 @@ if IsServer() then
 		else
 			caster:RemoveModifierByName("modifier_sai_immortal")
 		end
+	end
+end
+
+modifier_sai_immortal = class({
+	GetEffectName = function() return "particles/units/heroes/hero_omniknight/omniknight_guardian_angel_wings.vpcf" end,
+	})
+
+function modifier_sai_immortal:IsHidden()
+	return true
+end
+
+function modifier_sai_immortal:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT,
+		MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE,
+		MODIFIER_PROPERTY_TOTALDAMAGEOUTGOING_PERCENTAGE,
+		MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE
+	}
+end
+
+function modifier_sai_immortal:GetModifierAttackSpeedBonus_Constant()
+		return self:GetAbility():GetSpecialValueFor("attack_speed_debuff")
+end
+
+function modifier_sai_immortal:GetModifierMoveSpeedBonus_Percentage()
+	local ability = self:GetAbility()
+	local debuff = ability:GetSpecialValueFor("decreasement_ms_pct")
+	return debuff
+end
+
+function modifier_sai_immortal:GetModifierTotalDamageOutgoing_Percentage()
+	local ability = self:GetAbility()
+	local debuff = ability:GetSpecialValueFor("decreasement_pct")
+	return debuff
+end
+
+function modifier_sai_immortal:GetModifierIncomingDamage_Percentage()
+		return -self:GetAbility():GetSpecialValueFor("damage_reduction_pct")
+end
+
+if IsServer() then
+	function modifier_sai_immortal:OnCreated()
+		self:StartIntervalThink(self:GetAbility():GetSpecialValueFor("think_interval"))
+		self:OnIntervalThink()
+	end
+end
+
+function modifier_sai_immortal:OnIntervalThink()
+	local ability = self:GetAbility()
+	local parent = self:GetParent()
+	local caster = self:GetCaster()
+	if parent:GetMana() > ability:GetSpecialValueFor("mana_per_second") and ability:GetToggleState() then
+		parent:SpendMana(self:GetAbility():GetSpecialValueFor("mana_per_second") *
+		ability:GetSpecialValueFor("think_interval"), ability)
+	else 
+		ability:ToggleAbility()
+		caster:RemoveModifierByName("modifier_sai_immortal")
 	end
 end
