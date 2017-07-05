09fdbce8962e913c881104cb523626a1fbd24b2f
XELLA\Александр <fackyouprodaction@gmail.com>
2017-07-05 22:09:47 +0300

    -Serious punch have a PURE damage now and take 100% hero damage.

--- game/scripts/vscripts/heroes/hero_saitama/serious_punch.lua
+++ game/scripts/vscripts/heroes/hero_saitama/serious_punch.lua
@@ -4,8 +4,7 @@ if IsServer() then
 	function saitama_serios_punch:OnSpellStart()
 		local caster = self:GetCaster()
 		local target = self:GetCursorTarget()
-		local damage = caster:GetAverageTrueAttackDamage(target) * (0.01 * (self:GetSpecialValueFor("default_critical_damage_pct") + self:GetSpecialValueFor("bonus_ctitical_damage_pct") * caster:GetModifierStackCount("modifier_saitama_limiter", caster)))
-		print(0.01 * (self:GetSpecialValueFor("default_critical_damage_pct") + self:GetSpecialValueFor("bonus_ctitical_damage_pct") * caster:GetModifierStackCount("modifier_saitama_limiter", caster)))
+		local damage = (0.01 * self:GetSpecialValueFor("used_damage_pct") * caster:GetAverageTrueAttackDamage(target)) * (0.01 * (self:GetSpecialValueFor("default_critical_damage_pct") + self:GetSpecialValueFor("bonus_ctitical_damage_pct") * caster:GetModifierStackCount("modifier_saitama_limiter", caster)))
 		ApplyDamage({
 			attacker = caster,
 			victim = target,
