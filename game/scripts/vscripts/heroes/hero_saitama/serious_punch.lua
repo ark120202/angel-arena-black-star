0a5d5f19f9a3cf66a3acc312727271c1fbfc5b90
ark120202 <ark120202@gmail.com>
2017-07-07 02:59:13 +0500

    Added Knockback to Serious Punch and slightly fixed localization

--- game/scripts/vscripts/heroes/hero_saitama/serious_punch.lua
+++ game/scripts/vscripts/heroes/hero_saitama/serious_punch.lua
@@ -4,11 +4,12 @@ if IsServer() then
 	function saitama_serios_punch:OnSpellStart()
 		local caster = self:GetCaster()
 		local target = self:GetCursorTarget()
-		local damage = (0.01 * self:GetSpecialValueFor("used_damage_pct") * caster:GetAverageTrueAttackDamage(target)) * (0.01 * (self:GetSpecialValueFor("default_critical_damage_pct") + self:GetSpecialValueFor("bonus_ctitical_damage_pct") * caster:GetModifierStackCount("modifier_saitama_limiter", caster)))
-		caster:EmitSound("Hero_Earthshaker.EchoSlam")
+		local damage = caster:GetAverageTrueAttackDamage(target) * (self:GetSpecialValueFor("base_damage_multiplier_pct") + self:GetSpecialValueFor("damage_multiplier_per_stack_pct") * caster:GetModifierStackCount("modifier_saitama_limiter", caster)) * 0.01
 		local radius = damage * 0.1
-		local pfx = ParticleManager:CreateParticle("particles/arena/units/heroes/hero_saitama/serios_punch.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
-		ParticleManager:SetParticleControlEnt(pfx, 0, target, PATTACH_ABSORIGIN_FOLLOW, "attach_origin", caster:GetOrigin(), true)
+
+		target:EmitSound("Hero_Earthshaker.EchoSlam")
+		ParticleManager:CreateParticle("particles/arena/units/heroes/hero_saitama/serios_punch.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+
 		ApplyDamage({
 			attacker = caster,
 			victim = target,
@@ -16,6 +17,20 @@ if IsServer() then
 			damage_type = self:GetAbilityDamageType(),
 			damage_flags = DOTA_DAMAGE_FLAG_NO_SPELL_AMPLIFICATION,
 			ability = self
+		})
+
+		local duration = self:GetSpecialValueFor("knockback_duration")
+		local sourcePos = caster:GetAbsOrigin()
+		target:RemoveModifierByName("modifier_knockback")
+		target:AddNewModifier(caster, self, "modifier_knockback", {
+			knockback_duration = duration,
+			knockback_distance = self:GetSpecialValueFor("knockback_distance"),
+			knockback_height = 1600,
+			should_stun = 1,
+			duration = duration,
+			center_x = sourcePos.x,
+			center_y = sourcePos.y,
+			center_z = sourcePos.z
 		})
 	end
 end
