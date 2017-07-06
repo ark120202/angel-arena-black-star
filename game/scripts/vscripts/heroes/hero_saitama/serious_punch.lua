7bfa7035c527a970904e4db9f46b628ad299abc8
XELLA\Александр <fackyouprodaction@gmail.com>
2017-07-06 11:23:30 +0300

    -Added song for serios punch

--- game/scripts/vscripts/heroes/hero_saitama/serious_punch.lua
+++ game/scripts/vscripts/heroes/hero_saitama/serious_punch.lua
@@ -5,6 +5,7 @@ if IsServer() then
 		local caster = self:GetCaster()
 		local target = self:GetCursorTarget()
 		local damage = (0.01 * self:GetSpecialValueFor("used_damage_pct") * caster:GetAverageTrueAttackDamage(target)) * (0.01 * (self:GetSpecialValueFor("default_critical_damage_pct") + self:GetSpecialValueFor("bonus_ctitical_damage_pct") * caster:GetModifierStackCount("modifier_saitama_limiter", caster)))
+		caster:EmitSound("Hero_Earthshaker.EchoSlam")
 		ApplyDamage({
 			attacker = caster,
 			victim = target,
