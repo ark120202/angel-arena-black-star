56622753ecba5acf36075c7e0a3f0f7b74470f85
ark120202 <ark120202@gmail.com>
2017-07-06 20:37:46 +0500

    Made Limiter sound work

--- game/scripts/vscripts/heroes/hero_saitama/limiter.lua
+++ game/scripts/vscripts/heroes/hero_saitama/limiter.lua
@@ -25,8 +25,8 @@ if IsServer() then
 			duration = 1.2, -- 36 / 30
 			activity = ACT_DOTA_CAST_ABILITY_6
 		})
-
 		caster:EmitSound("Arena.Hero_Saitama.Limiter")
+
 		caster:ModifyStrength(caster:GetStrength() * self:GetSpecialValueFor("bonus_strength_pct") * caster:GetModifierStackCount("modifier_saitama_limiter", caster) * 0.01)
 	end
 end
