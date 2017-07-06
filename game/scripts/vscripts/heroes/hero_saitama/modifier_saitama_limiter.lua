cbabeb92902cbe919126b7994b3fae473c7f9711
ark120202 <ark120202@gmail.com>
2017-07-06 20:03:58 +0500

    Limiter modifier now can get specials without Limiter ability

--- game/scripts/vscripts/heroes/hero_saitama/modifier_saitama_limiter.lua
+++ game/scripts/vscripts/heroes/hero_saitama/modifier_saitama_limiter.lua
@@ -11,10 +11,11 @@ if IsServer() then
 	function modifier_saitama_limiter:OnDeath(keys)
 		local caster = self:GetCaster()
 		local ability = caster:FindAbilityByName("saitama_limiter")
+		local level = ability and math.max(ability:GetLevel(), 1) or 1
 		if keys.attacker == caster and keys.unit:IsTrueHero() then
-			self:SetStackCount(self:GetStackCount() + ability:GetLevelSpecialValueFor("stacks_for_kill", math.max(ability:GetLevel(), 1)))
+			self:SetStackCount(self:GetStackCount() + GetAbilitySpecial("saitama_limiter", "stacks_for_kill", level))
 		elseif keys.unit == caster then
-			self:SetStackCount(math.ceil(self:GetStackCount() * (1 - ability:GetLevelSpecialValueFor("loss_stacks_pct", math.max(ability:GetLevel(), 1)) * 0.01)))
+			self:SetStackCount(math.ceil(self:GetStackCount() * (1 - GetAbilitySpecial("saitama_limiter", "loss_stacks_pct", level) * 0.01)))
 		end
 	end
 end
