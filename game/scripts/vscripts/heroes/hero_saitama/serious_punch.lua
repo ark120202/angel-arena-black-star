802844e2c4ba09af4b63251cdb9f148a49df3098
XELLA\Александр <fackyouprodaction@gmail.com>
2017-07-06 15:37:24 +0300

    -Add animation and sond for Serios Punch

--- game/scripts/vscripts/heroes/hero_saitama/serious_punch.lua
+++ game/scripts/vscripts/heroes/hero_saitama/serious_punch.lua
@@ -6,6 +6,9 @@ if IsServer() then
 		local target = self:GetCursorTarget()
 		local damage = (0.01 * self:GetSpecialValueFor("used_damage_pct") * caster:GetAverageTrueAttackDamage(target)) * (0.01 * (self:GetSpecialValueFor("default_critical_damage_pct") + self:GetSpecialValueFor("bonus_ctitical_damage_pct") * caster:GetModifierStackCount("modifier_saitama_limiter", caster)))
 		caster:EmitSound("Hero_Earthshaker.EchoSlam")
+		local radius = damage * 0.1
+		local pfx = ParticleManager:CreateParticle("particles/arena/units/heroes/hero_saitama/serios_punch.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+		ParticleManager:SetParticleControlEnt(pfx, 0, target, PATTACH_ABSORIGIN_FOLLOW, "attach_origin", caster:GetOrigin(), true)
 		ApplyDamage({
 			attacker = caster,
 			victim = target,
