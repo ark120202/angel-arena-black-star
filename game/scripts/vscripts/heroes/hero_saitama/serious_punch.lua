fae757eb44a793f9141526876303102d319ffd4b
XELLA\Александр <fackyouprodaction@gmail.com>
2017-07-07 14:49:13 +0300

    -Now knockback from Serios Punch depends on the damage.

--- game/scripts/vscripts/heroes/hero_saitama/serious_punch.lua
+++ game/scripts/vscripts/heroes/hero_saitama/serious_punch.lua
@@ -5,7 +5,6 @@ if IsServer() then
 		local caster = self:GetCaster()
 		local target = self:GetCursorTarget()
 		local damage = caster:GetAverageTrueAttackDamage(target) * (self:GetSpecialValueFor("base_damage_multiplier_pct") + self:GetSpecialValueFor("damage_multiplier_per_stack_pct") * caster:GetModifierStackCount("modifier_saitama_limiter", caster)) * 0.01
-		local radius = damage * 0.1
 
 		target:EmitSound("Hero_Earthshaker.EchoSlam")
 		ParticleManager:CreateParticle("particles/arena/units/heroes/hero_saitama/serios_punch.vpcf", PATTACH_ABSORIGIN, target)
@@ -18,19 +17,20 @@ if IsServer() then
 			damage_flags = DOTA_DAMAGE_FLAG_NO_SPELL_AMPLIFICATION,
 			ability = self
 		})
-
-		local duration = self:GetSpecialValueFor("knockback_duration")
-		local sourcePos = caster:GetAbsOrigin()
-		target:RemoveModifierByName("modifier_knockback")
-		target:AddNewModifier(caster, self, "modifier_knockback", {
-			knockback_duration = duration,
-			knockback_distance = self:GetSpecialValueFor("knockback_distance"),
-			knockback_height = 1600,
-			should_stun = 1,
-			duration = duration,
-			center_x = sourcePos.x,
-			center_y = sourcePos.y,
-			center_z = sourcePos.z
-		})
+		if damage > 0 then
+			local sourcePos = caster:GetAbsOrigin()
+			local duration =  damage/6000
+			target:RemoveModifierByName("modifier_knockback")
+			target:AddNewModifier(caster, self, "modifier_knockback", {
+				knockback_duration = duration,
+				knockback_distance = damage/10,
+				knockback_height = damage/50,
+				should_stun = 1,
+				duration = duration,
+				center_x = sourcePos.x,
+				center_y = sourcePos.y,
+				center_z = sourcePos.z
+			})
+		end
 	end
 end
