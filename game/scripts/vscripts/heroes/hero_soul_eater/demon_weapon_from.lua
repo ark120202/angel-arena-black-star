93d77764442dc6c785dce24781f4869a79fb6af4
ark120202 <ark120202@gmail.com>
2017-07-18 12:43:45 +0500

    Fixed Demon Weapon Form

--- game/scripts/vscripts/heroes/hero_soul_eater/demon_weapon_from.lua
+++ game/scripts/vscripts/heroes/hero_soul_eater/demon_weapon_from.lua
@@ -1,26 +1,27 @@
+LinkLuaModifier("modifier_soul_eater_demon_weapon_from", "heroes/hero_soul_eater/demon_weapon_from.lua", LUA_MODIFIER_MOTION_NONE)
+
 soul_eater_demon_weapon_from = class({})
-LinkLuaModifier("modifier_soul_eater_demon_weapon_from", "heroes/hero_soul_eater/modifier_soul_eater_demon_weapon_from.lua", LUA_MODIFIER_MOTION_NONE)
 
 if IsServer() then
 	function soul_eater_demon_weapon_from:CastFilterResult()
-		local parent = self:GetParent()
-		local maka = parent:GetLinkedHeroEntities()[1]
-		local parentabs = parent:GetAbsOrigin()
-		return (maka:GetAbsOrigin() - parentabs):Length2D() <= self:GetCastRange(parentabs, maka) and UF_FAIL_INVALID_LOCATION or UF_SUCCESS
+		local soul = self:GetCaster()
+		local maka = soul:GetLinkedHeroEntities()[1]
+		local position = soul:GetAbsOrigin()
+		return (maka:GetAbsOrigin() - position):Length2D() <= self:GetCastRange(position, maka) and UF_FAIL_INVALID_LOCATION or UF_SUCCESS
 	end
 
 	--[[function soul_eater_demon_weapon_from:GetCustomCastError()
-		local parent = self:GetParent()
-		local maka = parent:GetLinkedHeroEntities()[1]
-		local parentabs = parent:GetAbsOrigin()
-		return (maka:GetAbsOrigin() - parentabs):Length2D() <= self:GetCastRange(parentabs, maka) and "#dota_hud_error_target_out_of_range" or ""
+		local soul = self:GetCaster()
+		local maka = soul:GetLinkedHeroEntities()[1]
+		local position = soul:GetAbsOrigin()
+		return (maka:GetAbsOrigin() - position):Length2D() <= self:GetCastRange(position, maka) and "#dota_hud_error_target_out_of_range" or ""
 	end]]
 
 	function soul_eater_demon_weapon_from:OnSpellStart()
-		local parent = self:GetParent()
-		local maka = parent:GetLinkedHeroEntities()[1]
-		local parentabs = parent:GetAbsOrigin()
-		if maka and (maka:GetAbsOrigin() - parentabs):Length2D() <= self:GetCastRange(parentabs, maka) then
+		local soul = self:GetCaster()
+		local maka = soul:GetLinkedHeroEntities()[1]
+		local position = soul:GetAbsOrigin()
+		if maka and (maka:GetAbsOrigin() - position):Length2D() <= self:GetCastRange(position, maka) then
 			local scythe = maka:GetWearableInSlot("weapon")
 			if scythe then
 				local scythe_ent = scythe.entity
@@ -28,7 +29,54 @@ if IsServer() then
 					scythe_ent:SetVisible(true)
 				end
 			end
-			caster:AddNewModifier(maka, self, "modifier_soul_eater_demon_weapon_from", nil)
+			soul:AddNewModifier(maka, self, "modifier_soul_eater_demon_weapon_from", nil)
 		end
+	end
+end
+
+
+modifier_soul_eater_demon_weapon_from = class({})
+
+function modifier_soul_eater_demon_weapon_from:CheckState()
+	return {
+		--[MODIFIER_STATE_DISARMED] = true,
+		[MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+		[MODIFIER_STATE_NOT_ON_MINIMAP] = true,
+		[MODIFIER_STATE_UNSELECTABLE] = true,
+		[MODIFIER_STATE_OUT_OF_GAME] = true,
+		[MODIFIER_STATE_NO_HEALTH_BAR] = true,
+		[MODIFIER_STATE_INVULNERABLE] = true,
+	}
+end
+
+if IsServer() then
+	function modifier_soul_eater_demon_weapon_from:OnCreated()
+		local soul = self:GetParent()
+		local maka = self:GetCaster()
+		soul:AddNoDraw()
+		self:StartIntervalThink(1/30)
+		soul:SwapAbilities("soul_eater_demon_weapon_from", "soul_eater_human_form", false, true)
+	end
+
+	function modifier_soul_eater_demon_weapon_from:OnIntervalThink()
+		local soul = self:GetParent()
+		local maka = self:GetCaster()
+		soul:SetAbsOrigin(maka:GetAbsOrigin())
+		if not maka:IsAlive() then
+			self:Destroy()
+		end
+	end
+	function modifier_soul_eater_demon_weapon_from:OnDestroy()
+		local soul = self:GetParent()
+		soul:RemoveNoDraw()
+		local maka = self:GetCaster()
+		local scythe = maka:GetWearableInSlot("weapon")
+		if scythe then
+			local scythe_ent = scythe.entity
+			if IsValidEntity(scythe_ent) then
+				scythe_ent:SetVisible(false)
+			end
+		end
+		soul:SwapAbilities("soul_eater_demon_weapon_from", "soul_eater_human_form", true, false)
 	end
 end
