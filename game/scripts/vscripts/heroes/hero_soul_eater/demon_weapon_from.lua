440941be1576d9420b58f106d9d62f65327f3137
ark120202 <ark120202@gmail.com>
2017-07-22 14:42:55 +0500

    Added projectiles

--- game/scripts/vscripts/heroes/hero_soul_eater/demon_weapon_from.lua
+++ game/scripts/vscripts/heroes/hero_soul_eater/demon_weapon_from.lua
@@ -1,4 +1,5 @@
 LinkLuaModifier("modifier_soul_eater_demon_weapon_from", "heroes/hero_soul_eater/demon_weapon_from.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_soul_eater_demon_weapon_from_transformation", "heroes/hero_soul_eater/demon_weapon_from.lua", LUA_MODIFIER_MOTION_NONE)
 
 soul_eater_demon_weapon_from = class({})
 
@@ -22,20 +23,64 @@ if IsServer() then
 		local maka = soul:GetLinkedHeroEntities()[1]
 		local position = soul:GetAbsOrigin()
 		if maka and (maka:GetAbsOrigin() - position):Length2D() <= self:GetCastRange(position, maka) then
-			local scythe = maka:GetWearableInSlot("weapon")
-			if scythe then
-				local scythe_ent = scythe.entity
-				if IsValidEntity(scythe_ent) then
-					scythe_ent:SetVisible(true)
-				end
-			end
-			soul:AddNewModifier(maka, self, "modifier_soul_eater_demon_weapon_from", nil)
+			self.maka = maka
+			local team = soul:GetTeamNumber()
+
+			soul:AddNewModifier(maka, self, "modifier_soul_eater_demon_weapon_from_transformation", nil)
+			ProjectileManager:CreateTrackingProjectile({
+				Target = maka,
+				Source = soul,
+				Ability = self,
+				EffectName = "particles/units/heroes/hero_arc_warden/arc_warden_wraith_prj.vpcf",
+				iMoveSpeed = self:GetAbilitySpecial("projectile_speed"),
+				bProvidesVision = true,
+				iVisionRadius = self:GetAbilitySpecial("vision_radius"),
+				iVisionTeamNumber = team,
+				iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_HITLOCATION
+			})
 		end
+	end
+
+	function soul_eater_demon_weapon_from:OnProjectileHit(hTarget, vLocation)
+		local soul = self:GetCaster()
+		soul:RemoveModifierByName("modifier_soul_eater_demon_weapon_from_transformation")
+		soul:AddNewModifier(self.maka, self, "modifier_soul_eater_demon_weapon_from", nil)
 	end
 end
 
 
-modifier_soul_eater_demon_weapon_from = class({})
+modifier_soul_eater_demon_weapon_from_transformation = class({
+	IsPurgable = function() return false end,
+	IsHidden   = function() return true end,
+})
+
+function modifier_soul_eater_demon_weapon_from_transformation:CheckState()
+	return {
+		[MODIFIER_STATE_STUNNED] = true,
+		[MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+		[MODIFIER_STATE_NOT_ON_MINIMAP] = true,
+		[MODIFIER_STATE_UNSELECTABLE] = true,
+		[MODIFIER_STATE_OUT_OF_GAME] = true,
+		[MODIFIER_STATE_NO_HEALTH_BAR] = true,
+		[MODIFIER_STATE_INVULNERABLE] = true,
+	}
+end
+
+if IsServer() then
+	function modifier_soul_eater_demon_weapon_from_transformation:OnCreated()
+		self:GetParent():AddNoDraw()
+	end
+
+	function modifier_soul_eater_demon_weapon_from_transformation:OnDestroy()
+		self:GetParent():RemoveNoDraw()
+	end
+end
+
+
+modifier_soul_eater_demon_weapon_from = class({
+	IsPurgable = function() return false end,
+	IsHidden   = function() return true end,
+})
 
 function modifier_soul_eater_demon_weapon_from:CheckState()
 	return {
@@ -53,6 +98,14 @@ if IsServer() then
 	function modifier_soul_eater_demon_weapon_from:OnCreated()
 		local soul = self:GetParent()
 		local maka = self:GetCaster()
+		local scythe = maka:GetWearableInSlot("weapon")
+		if scythe then
+			local scythe_ent = scythe.entity
+			if IsValidEntity(scythe_ent) then
+				scythe_ent:SetVisible(true)
+			end
+		end
+
 		soul:AddNoDraw()
 		self:StartIntervalThink(1/30)
 		soul:SwapAbilities("soul_eater_demon_weapon_from", "soul_eater_human_form", false, true)
--- game/scripts/vscripts/heroes/hero_soul_eater/human_form.lua
+++ game/scripts/vscripts/heroes/hero_soul_eater/human_form.lua
@@ -1,12 +1,64 @@
+LinkLuaModifier("modifier_soul_eater_human_form_transformation", "heroes/hero_soul_eater/human_form.lua", LUA_MODIFIER_MOTION_NONE)
+
 soul_eater_human_form = class({})
 
 if IsServer() then
 	function soul_eater_human_form:OnSpellStart()
-		local caster = self:GetCaster()
-		local modifier = caster:FindModifierByName("modifier_soul_eater_demon_weapon_from")
-		local maka = modifier:GetCaster()
-		modifier:Destroy()
+		local soul = self:GetCaster()
 		local point = self:GetCursorPosition()
-		
+		local soulPos = soul:GetAbsOrigin()
+		local modifier = soul:FindModifierByName("modifier_soul_eater_demon_weapon_from")
+		modifier:Destroy()
+
+		local team = soul:GetTeamNumber()
+		self.dummy = CreateUnitByName("npc_dummy_unit", point, false, soul, soul, team)
+
+		soul:AddNewModifier(soul, self, "modifier_soul_eater_human_form_transformation", nil)
+		ProjectileManager:CreateTrackingProjectile({
+			Target = self.dummy,
+			Source = soul,
+			Ability = self,
+			EffectName = "particles/units/heroes/hero_arc_warden/arc_warden_wraith_prj.vpcf",
+			iMoveSpeed = self:GetAbilitySpecial("projectile_speed"),
+			bProvidesVision = true,
+			iVisionRadius = self:GetAbilitySpecial("vision_radius"),
+			iVisionTeamNumber = team,
+			iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_HITLOCATION
+		})
+	end
+
+	function soul_eater_human_form:OnProjectileHit(hTarget, vLocation)
+		local soul = self:GetCaster()
+		UTIL_Remove(self.dummy)
+		soul:RemoveModifierByName("modifier_soul_eater_human_form_transformation")
+		FindClearSpaceForUnit(soul, vLocation, true)
+	end
+end
+
+
+modifier_soul_eater_human_form_transformation = class({
+	IsPurgable = function() return false end,
+	IsHidden   = function() return true end,
+})
+
+function modifier_soul_eater_human_form_transformation:CheckState()
+	return {
+		[MODIFIER_STATE_STUNNED] = true,
+		[MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+		[MODIFIER_STATE_NOT_ON_MINIMAP] = true,
+		[MODIFIER_STATE_UNSELECTABLE] = true,
+		[MODIFIER_STATE_OUT_OF_GAME] = true,
+		[MODIFIER_STATE_NO_HEALTH_BAR] = true,
+		[MODIFIER_STATE_INVULNERABLE] = true,
+	}
+end
+
+if IsServer() then
+	function modifier_soul_eater_human_form_transformation:OnCreated()
+		self:GetParent():AddNoDraw()
+	end
+
+	function modifier_soul_eater_human_form_transformation:OnDestroy()
+		self:GetParent():RemoveNoDraw()
 	end
 end
