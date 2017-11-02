ccc253b7ae7fe637fd750738646e070f1900f8c7
Arkady Pozdnyakov <ark120202@gmail.com>
2017-11-02 22:05:32 +0500

    feat(panorama_shop): search now searches only through name, localized name and aliases

--- game/scripts/vscripts/data/kv_data.lua
+++ game/scripts/vscripts/data/kv_data.lua
@@ -4,9 +4,3 @@ table.deepmerge(NPC_HEROES_CUSTOM, LoadKeyValues("scripts/npc/heroes/new.txt"))
 NPC_HEROES = LoadKeyValues("scripts/npc/npc_heroes.txt")
 --HEROLIST = LoadKeyValues("scripts/npc/herolist.txt")
 ENABLED_HEROES = LoadKeyValues("scripts/npc/custom_herolist.txt")
-
-LANG_ENGLISH = {}
-table.merge(LANG_ENGLISH, LoadKeyValues("resource/addon_english.txt")["Tokens"])
-table.merge(LANG_ENGLISH, LoadKeyValues("scripts/npc/KV/dota_items_english.txt")["Tokens"])
-LANG_RUSSIAN = {}
-table.merge(LANG_RUSSIAN, LoadKeyValues("scripts/npc/KV/dota_items_russian.txt")["Tokens"])
