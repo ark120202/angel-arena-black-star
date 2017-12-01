7fae87249e1e6252b51d15e7b333c01ed70fb872
Arkady Pozdnyakov <ark120202@gmail.com>
2017-12-01 16:52:18 +0500

    chore(SimpleAI): remove constant moving to spawn position in returning state

--- game/scripts/vscripts/modules/SimpleAI/SimpleAI.lua
+++ game/scripts/vscripts/modules/SimpleAI/SimpleAI.lua
@@ -124,7 +124,6 @@ end
 	end
 
 	function SimpleAI:ReturningThink()
-		self.unit:MoveToPosition(self.spawnPos) -- If movement order was interrupted for some reason (Force Staff)
 		if (self.spawnPos - self.unit:GetAbsOrigin()):Length2D() < 10 then
 			self:SwitchState(AI_STATE_IDLE)
 			return
