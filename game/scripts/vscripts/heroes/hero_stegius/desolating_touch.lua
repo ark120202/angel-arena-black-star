efc4c794800cbd382e89055892b3014a8f7f46bc
XELLA\Александр <fackyouprodaction@gmail.com>
2017-12-29 18:44:02 +0300

    Optimization

--- game/scripts/vscripts/heroes/hero_stegius/desolating_touch.lua
+++ game/scripts/vscripts/heroes/hero_stegius/desolating_touch.lua
@@ -4,7 +4,7 @@ function ReduceArmor(keys)
 	local ability = keys.ability
 	local disarmor_pct = caster:GetTalentSpecial("talent_hero_stegius_desolating_touch", "armor_per_hit_pct") or 0
 	if not unit:IsBoss() or RollPercentage(ability:GetAbilitySpecial("boss_armor_reduction_chance_pct")) then
-		local stacks = -(ability:GetAbilitySpecial("armor_per_hit") - disarmor_pct * unit:GetPhysicalArmorBaseValue() * 0.01)
+		local stacks = ability:GetAbilitySpecial("armor_per_hit") + disarmor_pct * unit:GetPhysicalArmorBaseValue() * 0.01
 		ModifyStacks(ability, caster, unit, "modifier_stegius_desolating_touch_debuff", stacks, true)
 		unit:EmitSound("Item_Desolator.Target")
 		Timers:CreateTimer(ability:GetAbilitySpecial("duration"), function()
