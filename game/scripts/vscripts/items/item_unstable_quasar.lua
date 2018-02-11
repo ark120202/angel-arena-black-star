af7136e21856984eb875384e82401b4f7ff610b3
Arkady Pozdnyakov <ark120202@gmail.com>
2018-02-11 15:21:17 +0500

    style(items): replace spaces with tabs

--- game/scripts/vscripts/items/item_unstable_quasar.lua
+++ game/scripts/vscripts/items/item_unstable_quasar.lua
@@ -3,90 +3,90 @@ LinkLuaModifier("modifier_item_unstable_quasar_slow", "items/item_unstable_quasa
 LinkLuaModifier("modifier_item_unstable_quasar_aura", "items/item_unstable_quasar.lua", LUA_MODIFIER_MOTION_NONE)
 
 item_unstable_quasar = {
-    GetIntrinsicModifierName = function() return "modifier_item_unstable_quasar_passive" end,
+	GetIntrinsicModifierName = function() return "modifier_item_unstable_quasar_passive" end,
 }
 
 modifier_item_unstable_quasar_passive = class({
-    IsHidden   = function() return true end,
-    IsPurgable = function() return false end,
-    RemoveOnDeath = function() return false end,
+	IsHidden   = function() return true end,
+	IsPurgable = function() return false end,
+	RemoveOnDeath = function() return false end,
 })
 
 function modifier_item_unstable_quasar_passive:DeclareFunctions()
-    return {
-        MODIFIER_EVENT_ON_ABILITY_EXECUTED,
-        MODIFIER_PROPERTY_BONUS_DAY_VISION,
-        MODIFIER_PROPERTY_BONUS_NIGHT_VISION,
-        MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
-        MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE,
-        MODIFIER_PROPERTY_TOTALDAMAGEOUTGOING_PERCENTAGE,
-    }   
+	return {
+		MODIFIER_EVENT_ON_ABILITY_EXECUTED,
+		MODIFIER_PROPERTY_BONUS_DAY_VISION,
+		MODIFIER_PROPERTY_BONUS_NIGHT_VISION,
+		MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
+		MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE,
+		MODIFIER_PROPERTY_TOTALDAMAGEOUTGOING_PERCENTAGE,
+	}
 end
 
 modifier_item_unstable_quasar_slow = class({
-    IsHidden   = function() return false end,
-    IsPurgable = function() return true end,
-    IsDebuff = function() return true end,
-    GetEffectName = function() return "particles/items_fx/diffusal_slow.vpcf" end,
+	IsHidden   = function() return false end,
+	IsPurgable = function() return true end,
+	IsDebuff = function() return true end,
+	GetEffectName = function() return "particles/items_fx/diffusal_slow.vpcf" end,
 })
 
-function modifier_item_unstable_quasar_slow:DeclareFunctions() 
-    return { 
-        MODIFIER_PROPERTY_MOVESPEED_MAX,
-        MODIFIER_PROPERTY_MOVESPEED_LIMIT,
-        MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE
-    } 
+function modifier_item_unstable_quasar_slow:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_MOVESPEED_MAX,
+		MODIFIER_PROPERTY_MOVESPEED_LIMIT,
+		MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE
+	}
 end
 
 function item_unstable_quasar:GetManaCost(iLevel)
-    local caster = self:GetCaster()
-    if caster then 
-        return self:GetSpecialValueFor("manacost") + caster:GetMaxMana() * self:GetSpecialValueFor("manacost_pct") * 0.01
-    end
+	local caster = self:GetCaster()
+	if caster then
+		return self:GetSpecialValueFor("manacost") + caster:GetMaxMana() * self:GetSpecialValueFor("manacost_pct") * 0.01
+	end
 end
 
 if IsServer() then
-    function modifier_item_unstable_quasar_passive:OnAbilityExecuted(keys)
-        local caster = self:GetParent()
-        if caster ~= keys.unit then return end
-        local ability = self:GetAbility()
-        local usedAbility = keys.ability
-        local team = caster:GetTeam()
-        local pos = caster:GetAbsOrigin()
-        local radius = ability:GetSpecialValueFor("damage_radius")
+	function modifier_item_unstable_quasar_passive:OnAbilityExecuted(keys)
+		local caster = self:GetParent()
+		if caster ~= keys.unit then return end
+		local ability = self:GetAbility()
+		local usedAbility = keys.ability
+		local team = caster:GetTeam()
+		local pos = caster:GetAbsOrigin()
+		local radius = ability:GetSpecialValueFor("damage_radius")
 
-        if (
-            PreformAbilityPrecastActions(caster, ability) and
-            usedAbility:GetCooldown(usedAbility:GetLevel()) >= ability:GetSpecialValueFor("min_ability_cooldown") and 
-            usedAbility:GetManaCost(usedAbility:GetLevel()) ~= 0
-        ) then           
-            for _,v in ipairs(FindUnitsInRadius(team, pos, nil, radius, ability:GetAbilityTargetTeam(), ability:GetAbilityTargetType(), ability:GetAbilityTargetFlags(), FIND_ANY_ORDER, false)) do
-                local enemyPos = v:GetAbsOrigin()
-                local damage = v:GetHealth() * ability:GetSpecialValueFor(caster:GetPrimaryAttribute() == DOTA_ATTRIBUTE_INTELLECT and "intelligence_damage_pct" or "damage_pct") * 0.01 + ability:GetSpecialValueFor("base_damage")
+		if (
+			PreformAbilityPrecastActions(caster, ability) and
+			usedAbility:GetCooldown(usedAbility:GetLevel()) >= ability:GetSpecialValueFor("min_ability_cooldown") and
+			usedAbility:GetManaCost(usedAbility:GetLevel()) ~= 0
+		) then
+			for _,v in ipairs(FindUnitsInRadius(team, pos, nil, radius, ability:GetAbilityTargetTeam(), ability:GetAbilityTargetType(), ability:GetAbilityTargetFlags(), FIND_ANY_ORDER, false)) do
+				local enemyPos = v:GetAbsOrigin()
+				local damage = v:GetHealth() * ability:GetSpecialValueFor(caster:GetPrimaryAttribute() == DOTA_ATTRIBUTE_INTELLECT and "intelligence_damage_pct" or "damage_pct") * 0.01 + ability:GetSpecialValueFor("base_damage")
 
-                ApplyDamage({
-                    attacker = caster,
-                    victim = v,
-                    damage = damage,
-                    damage_type = ability:GetAbilityDamageType(),
-                    damage_flags = DOTA_DAMAGE_FLAG_NO_SPELL_AMPLIFICATION,
-                    ability = self:GetAbility()
-                })
-                
-                local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_zuus/zuus_arc_lightning.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
-                ParticleManager:SetParticleControl(particle, 1, enemyPos)
+				ApplyDamage({
+					attacker = caster,
+					victim = v,
+					damage = damage,
+					damage_type = ability:GetAbilityDamageType(),
+					damage_flags = DOTA_DAMAGE_FLAG_NO_SPELL_AMPLIFICATION,
+					ability = self:GetAbility()
+				})
 
-                v:AddNewModifier(caster, ability, "modifier_item_unstable_quasar_slow", {duration = ability:GetSpecialValueFor("slow_duration")})
+				local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_zuus/zuus_arc_lightning.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
+				ParticleManager:SetParticleControl(particle, 1, enemyPos)
 
-                local distance = pos - enemyPos
-                local offset = ability:GetSpecialValueFor("offset_distance")
+				v:AddNewModifier(caster, ability, "modifier_item_unstable_quasar_slow", {duration = ability:GetSpecialValueFor("slow_duration")})
 
-                FindClearSpaceForUnit(v, GetGroundPosition(enemyPos + distance:Normalized() * offset, caster), true)
+				local distance = pos - enemyPos
+				local offset = ability:GetSpecialValueFor("offset_distance")
 
-                --caster:EmitSound("Hero_Pugna.NetherWard")
-            end
-        end
-    end
+				FindClearSpaceForUnit(v, GetGroundPosition(enemyPos + distance:Normalized() * offset, caster), true)
+
+				--caster:EmitSound("Hero_Pugna.NetherWard")
+			end
+		end
+	end
 end
 
 function modifier_item_unstable_quasar_slow:GetModifierMoveSpeed_Limit()
@@ -103,23 +103,23 @@ end
 
 
 function modifier_item_unstable_quasar_passive:GetBonusDayVision()
-    return self:GetAbility():GetSpecialValueFor("bonus_day_vision")
+	return self:GetAbility():GetSpecialValueFor("bonus_day_vision")
 end
 
 function modifier_item_unstable_quasar_passive:GetBonusNightVision()
-    return self:GetAbility():GetSpecialValueFor("bonus_night_vision")
+	return self:GetAbility():GetSpecialValueFor("bonus_night_vision")
 end
 
 function modifier_item_unstable_quasar_passive:GetModifierBonusStats_Intellect()
-    return self:GetAbility():GetSpecialValueFor("bonus_intellect")
+	return self:GetAbility():GetSpecialValueFor("bonus_intellect")
 end
 
 function modifier_item_unstable_quasar_passive:GetModifierSpellAmplify_Percentage()
-    return self:GetAbility():GetSpecialValueFor("spell_amp_pct")
+	return self:GetAbility():GetSpecialValueFor("spell_amp_pct")
 end
 
 function modifier_item_unstable_quasar_passive:GetModifierTotalDamageOutgoing_Percentage()
-    return self:GetAbility():GetSpecialValueFor("increase_all_damage_pct")
+	return self:GetAbility():GetSpecialValueFor("increase_all_damage_pct")
 end
 
 function modifier_item_unstable_quasar_passive:GetModifierAura()
@@ -143,17 +143,17 @@ function modifier_item_unstable_quasar_passive:GetAuraSearchType()
 end
 
 modifier_item_unstable_quasar_aura = class({
-    DeclareFunctions = function() return { MODIFIER_PROPERTY_MAGICAL_RESISTANCE_BONUS } end,
-    GetPriority = function() return { MODIFIER_PRIORITY_HIGH } end,     
+	DeclareFunctions = function() return { MODIFIER_PROPERTY_MAGICAL_RESISTANCE_BONUS } end,
+	GetPriority = function() return { MODIFIER_PRIORITY_HIGH } end,
 })
 
 function modifier_item_unstable_quasar_aura:GetModifierMagicalResistanceBonus()
-    return -self:GetAbility():GetSpecialValueFor("aura_descrease_resistange_pct")
+	return -self:GetAbility():GetSpecialValueFor("aura_descrease_resistange_pct")
 end
 
 function modifier_item_unstable_quasar_aura:CheckState()
-    return {
-        [MODIFIER_STATE_INVISIBLE] = false,
-    }
+	return {
+		[MODIFIER_STATE_INVISIBLE] = false,
+	}
 end
 
