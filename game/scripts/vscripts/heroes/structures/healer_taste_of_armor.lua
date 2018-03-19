ba454cfd88e7652256fb48400608a5c3597f5174
shimaosu <gagorka60@gmail.com>
2018-03-19 20:24:10 +0200

    replaced spaces to tabs

--- game/scripts/vscripts/heroes/structures/healer_taste_of_armor.lua
+++ game/scripts/vscripts/heroes/structures/healer_taste_of_armor.lua
@@ -2,40 +2,40 @@ LinkLuaModifier("modifier_healer_taste_of_armor", "heroes/structures/healer_tast
 LinkLuaModifier("modifier_healer_taste_of_armor_effect", "heroes/structures/healer_taste_of_armor.lua",LUA_MODIFIER_MOTION_NONE)
 
 healer_taste_of_armor = class({
-    GetIntrinsicModifierName = function() return "modifier_healer_taste_of_armor" end,
+	GetIntrinsicModifierName = function() return "modifier_healer_taste_of_armor" end,
 })
 
 modifier_healer_taste_of_armor = class({
-    IsPurgable = function() return false end,
-    IsHidden = function() return true end,
+	IsPurgable = function() return false end,
+	IsHidden = function() return true end,
 })
 
 function modifier_healer_taste_of_armor:GetAuraRadius()
-    return self:GetAbility():GetSpecialValueFor("aura_radius")
+	return self:GetAbility():GetSpecialValueFor("aura_radius")
 end
 function modifier_healer_taste_of_armor:GetAuraSearchTeam()
-    return self:GetAbility():GetAbilityTargetTeam()
+	return self:GetAbility():GetAbilityTargetTeam()
 end
 function modifier_healer_taste_of_armor:GetAuraSearchType()
-    return self:GetAbility():GetAbilityTargetType()
+	return self:GetAbility():GetAbilityTargetType()
 end
 function modifier_healer_taste_of_armor:GetAuraSearchFlags()
-    return self:GetAbility():GetAbilityTargetFlags()
+	return self:GetAbility():GetAbilityTargetFlags()
 end
 function modifier_healer_taste_of_armor:IsAura()
 	return true
 end
 function modifier_healer_taste_of_armor:GetModifierAura()
-    return "modifier_healer_taste_of_armor_effect"
+	return "modifier_healer_taste_of_armor_effect"
 end
 
 
 modifier_healer_taste_of_armor_effect = class({})
 function modifier_healer_taste_of_armor_effect:DeclareFunctions()
-    return {
-        MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
-    }
+	return {
+		MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
+	}
 end
 function modifier_healer_taste_of_armor_effect:GetModifierPhysicalArmorBonus()
-    return self:GetAbility():GetSpecialValueFor("aura_bonus_armor")
+	return self:GetAbility():GetSpecialValueFor("aura_bonus_armor")
 end
