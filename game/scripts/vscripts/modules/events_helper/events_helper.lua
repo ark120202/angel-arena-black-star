bbc967d4d5142072abe67fc912b87aff87ef9b47
Arkady Pozdnyakov <ark120202@gmail.com>
2018-04-07 18:21:04 +0500

    refactor(events): refactor Events module
    * Use separate :Once function instead of boolean argument on :On
    * Once and On will return callback id's
    * Subscribers will be called in subscribe order, instead of reversed subscribe order
    * Register now works not like simple :On

--- game/scripts/vscripts/modules/events_helper/events_helper.lua
+++ game/scripts/vscripts/modules/events_helper/events_helper.lua
@@ -6,30 +6,34 @@ function Events:Emit(...)
 	local args = {...}
 	local event = table.remove(args, 1)
 	local callbacks = Events.Callbacks[event]
-	if callbacks then
-		for i = #callbacks, 1, -1 do
-			local v = callbacks[i]
-			v.call(unpack(args))
-			if v.once then table.remove(callbacks, i) end
-		end
+	print("EMITTED EVENT " .. event .. " with first arg: ", args[0])
+	if not callbacks then return end
+
+	for i, v in pairs(callbacks) do
+		print(" ... and called callback")
+		v.call(unpack(args))
+		if v.once then callbacks[i] = nil end
 	end
-	return call
 end
 
-function Events:On(event, callback, once)
-	if once == nil then once = false end
-
+function Events:On(event, callback)
 	if not Events.Callbacks[event] then Events.Callbacks[event] = {} end
-	table.insert(Events.Callbacks[event], {call = callback, once = once})
+	table.insert(Events.Callbacks[event], { call = callback, once = false })
+	return #Events.Callbacks[event]
+end
+
+function Events:Once(event, callback)
+	if not Events.Callbacks[event] then Events.Callbacks[event] = {} end
+	table.insert(Events.Callbacks[event], { call = callback, once = true })
+	return #Events.Callbacks[event]
 end
 
 function Events:Register(event, listener, callback)
 	local uniqueName = event .. "_" ..listener
 
 	-- Remove registered callback if it was found
-	if Events.Registered[uniqueName] then
-		table.removeByValue(Events.Callbacks[event], Events.Registered[uniqueName])
-	end
-	Events.Registered[uniqueName] = callback
-	self:On(event, callback)
+	local registeredIndex = Events.Registered[uniqueName]
+	if registeredIndex then Events.Callbacks[event][registeredIndex] = nil end
+
+	Events.Registered[uniqueName] = self:On(event, callback)
 end
