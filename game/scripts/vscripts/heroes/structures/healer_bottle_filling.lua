f072133ef9c180e3996f582093c4042071eb38eb
shimaosu <gagorka60@gmail.com>
2018-05-30 17:33:59 +0300

    style(ability) caster replaced to parent

--- game/scripts/vscripts/heroes/structures/healer_bottle_filling.lua
+++ game/scripts/vscripts/heroes/structures/healer_bottle_filling.lua
@@ -17,18 +17,18 @@ if IsServer() then
 	end
 
 	function modifier_healer_bottle_filling:OnIntervalThink()
-		local caster = self:GetCaster()
+		local parent = self:GetParent()
 		local ability = self:GetAbility()
 		local radius = ability:GetSpecialValueFor("aura_radius")
 		local delay_duration = ability:GetSpecialValueFor('bottle_refill_cooldown')
-		for _,v in ipairs(FindUnitsInRadius(caster:GetTeamNumber(), caster:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)) do
+		for _,v in ipairs(FindUnitsInRadius(parent:GetTeamNumber(), parent:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)) do
 			if not v:HasModifier("modifier_healer_bottle_filling_delay") then
 				for i = 0, 11 do
 					local item = v:GetItemInSlot(i)
 					if item and item:GetAbilityName() == "item_bottle_arena" then
 						item:SetCurrentCharges(3)
 						v:EmitSound("DOTA_Item.MagicWand.Activate")
-						v:AddNewModifier(ability:GetCaster(), ability, "modifier_healer_bottle_filling_delay", {duration = delay_duration})
+						v:AddNewModifier(parent, ability, "modifier_healer_bottle_filling_delay", {duration = delay_duration})
 					end
 				end
 			end
@@ -40,4 +40,3 @@ modifier_healer_bottle_filling_delay = class({
 	IsDebuff = function() return true end,
 	IsPurgable = function() return false end,
 })
-
