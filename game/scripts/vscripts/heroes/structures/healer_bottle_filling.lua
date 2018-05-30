c8f56589f724fa42b06d07b29c2d79822705e4d6
shimaosu <gagorka60@gmail.com>
2018-05-30 13:14:14 +0300

    fix(abilities): shrine bottle filling ability worked incorrectly

--- game/scripts/vscripts/heroes/structures/healer_bottle_filling.lua
+++ game/scripts/vscripts/heroes/structures/healer_bottle_filling.lua
@@ -1,6 +1,6 @@
 LinkLuaModifier("modifier_healer_bottle_filling", "heroes/structures/healer_bottle_filling.lua", LUA_MODIFIER_MOTION_NONE)
-LinkLuaModifier("modifier_healer_bottle_filling_effect", "heroes/structures/healer_bottle_filling.lua", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_healer_bottle_filling_delay", "heroes/structures/healer_bottle_filling.lua", LUA_MODIFIER_MOTION_NONE)
+
 healer_bottle_filling = class({
 	GetIntrinsicModifierName = function() return "modifier_healer_bottle_filling" end,
 })
@@ -10,44 +10,33 @@ modifier_healer_bottle_filling = class({
 	IsHidden = function() return true end,
 })
 
-function modifier_healer_bottle_filling:GetAuraRadius()
-	return self:GetAbility():GetSpecialValueFor("aura_radius")
-end
-function modifier_healer_bottle_filling:GetAuraSearchTeam()
-	return self:GetAbility():GetAbilityTargetTeam()
-end
-function modifier_healer_bottle_filling:GetAuraSearchType()
-	return self:GetAbility():GetAbilityTargetType()
-end
-function modifier_healer_bottle_filling:GetAuraSearchFlags()
-	return self:GetAbility():GetAbilityTargetFlags()
-end
-function modifier_healer_bottle_filling:IsAura()
-	return true
-end
-function modifier_healer_bottle_filling:GetModifierAura()
-	return "modifier_healer_bottle_filling_effect"
-end
-
-modifier_healer_bottle_filling_effect = class({
-	IsHidden = function() return true end,
-	IsPurgable = function() return false end,
-})
-
 if IsServer() then
-	function modifier_healer_bottle_filling_effect:OnCreated()
-		local parent = self:GetParent()
-		for i = 0, 11 do
-			local item = parent:GetItemInSlot(i)
-			if item and item:GetAbilityName() == "item_bottle_arena" then
-				if  parent:IsCourier() or parent:HasModifier("modifier_healer_bottle_filling_delay") then return end
-				item:SetCurrentCharges(3)
-				local duration = self:GetAbility():GetSpecialValueFor('bottle_refill_cooldown')
-				local ability = self:GetAbility()
-				parent:AddNewModifier(ability:GetCaster(), ability, "modifier_healer_bottle_filling_delay", {duration = duration})
+	function modifier_healer_bottle_filling:OnCreated()
+		self:StartIntervalThink(1)
+		self:OnIntervalThink()
+	end
+
+	function modifier_healer_bottle_filling:OnIntervalThink()
+		local caster = self:GetCaster()
+		local ability = self:GetAbility()
+		local modifier = "modifier_healer_bottle_filling_delay"
+		local radius = ability:GetSpecialValueFor("aura_radius")
+		local delay_duration = ability:GetSpecialValueFor('bottle_refill_cooldown')
+		for _,v in ipairs(FindUnitsInRadius(caster:GetTeamNumber(), caster:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)) do
+			if v:HasModifier("modifier_healer_bottle_filling_delay") then return end
+			for i = 0, 11 do
+				local item = v:GetItemInSlot(i)
+				if item and item:GetAbilityName() == "item_bottle_arena" then
+					item:SetCurrentCharges(item:GetCurrentCharges()+1)
+					v:EmitSound("DOTA_Item.MagicWand.Activate")
+					if item:GetCurrentCharges() == 3 then
+						v:AddNewModifier(ability:GetCaster(), ability, "modifier_healer_bottle_filling_delay", {duration = delay_duration})
+					end
+				end
 			end
 		end
 	end
+
 end
 
 modifier_healer_bottle_filling_delay = class({
