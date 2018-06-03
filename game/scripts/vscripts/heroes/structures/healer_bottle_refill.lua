30bd6133b1f2131478042d96ef43c6b6176f1fc6
Arkady Pozdnyakov <ark120202@gmail.com>
2018-06-03 11:46:56 +0500

    chore(abilities): rename Bottle Refill delay to cooldown

--- game/scripts/vscripts/heroes/structures/healer_bottle_refill.lua
+++ game/scripts/vscripts/heroes/structures/healer_bottle_refill.lua
@@ -1,5 +1,5 @@
 LinkLuaModifier("modifier_healer_bottle_refill", "heroes/structures/healer_bottle_refill.lua", LUA_MODIFIER_MOTION_NONE)
-LinkLuaModifier("modifier_healer_bottle_refill_delay", "heroes/structures/healer_bottle_refill.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_healer_bottle_refill_cooldown", "heroes/structures/healer_bottle_refill.lua", LUA_MODIFIER_MOTION_NONE)
 
 healer_bottle_refill = class({
 	GetIntrinsicModifierName = function() return "modifier_healer_bottle_refill" end,
@@ -20,9 +20,9 @@ if IsServer() then
 		local parent = self:GetParent()
 		local ability = self:GetAbility()
 		local radius = ability:GetSpecialValueFor("aura_radius")
-		local delay_duration = ability:GetSpecialValueFor('bottle_refill_cooldown')
+		local cooldown = ability:GetSpecialValueFor('bottle_refill_cooldown')
 		for _,v in ipairs(FindUnitsInRadius(parent:GetTeamNumber(), parent:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)) do
-			if not v:HasModifier("modifier_healer_bottle_refill_delay") then
+			if not v:HasModifier("modifier_healer_bottle_refill_cooldown") then
 				local refilled = false
 				for i = 0, 11 do
 					local item = v:GetItemInSlot(i)
@@ -34,14 +34,14 @@ if IsServer() then
 
 				if refilled then
 					v:EmitSound("DOTA_Item.MagicWand.Activate")
-					v:AddNewModifier(parent, ability, "modifier_healer_bottle_refill_delay", {duration = delay_duration})
+					v:AddNewModifier(parent, ability, "modifier_healer_bottle_refill_cooldown", {duration = cooldown})
 				end
 			end
 		end
 	end
 end
 
-modifier_healer_bottle_refill_delay = class({
+modifier_healer_bottle_refill_cooldown = class({
 	IsDebuff = function() return true end,
 	IsPurgable = function() return false end,
 })
