290eb04c58540494ae89d72be13e643272a6b4bb
Arkady Pozdnyakov <ark120202@gmail.com>
2018-06-05 16:17:44 +0500

    fix(items): make Golden Arrow correctly not usable nearby the fountain

--- game/scripts/vscripts/items/item_golden_arrow.lua
+++ game/scripts/vscripts/items/item_golden_arrow.lua
@@ -6,6 +6,21 @@ item_golden_arrow = class({
 })
 
 if IsServer() then
+	function item_golden_arrow:HasEnoughAttributes()
+		local caster = self:GetCaster()
+		return caster:GetStrength() > 5 and caster:GetAgility() > 5 and caster:GetIntellect() > 5
+	end
+
+	function item_golden_arrow:CastFilterResult(hTarget)
+		local caster = self:GetCaster()
+		return self:HasEnoughAttributes() or not caster:HasModifier("modifier_fountain_aura_arena") and UF_SUCCESS or UF_FAIL_CUSTOM
+	end
+
+	function item_golden_arrow:GetCustomCastError(hTarget)
+		local caster = self:GetCaster()
+		return caster:HasModifier("modifier_fountain_aura_arena") and "#arena_hud_error_cant_cast_on_fountain" or ""
+	end
+
 	function item_golden_arrow:OnSpellStart()
 		local caster = self:GetCaster()
 		local modifier = caster:FindModifierByName("modifier_item_golden_arrow_counter")
@@ -25,12 +40,12 @@ if IsServer() then
 		ParticleManager:SetParticleControlEnt(particle, 0, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), true)
 
 		if caster:IsRealHero() then
-			if caster:GetStrength() > 5 and caster:GetAgility() > 5 and caster:GetIntellect() > 5 then
+			if self:HasEnoughAttributes() then
 				caster:ModifyStrength(stats_debuff_per_stack)
 				caster:ModifyAgility(stats_debuff_per_stack)
 				caster:ModifyIntellect(stats_debuff_per_stack)
 				Gold:AddGoldWithMessage(caster, new_gold)
-			elseif not caster:HasModifier("modifier_fountain_aura_arena") then
+			else
 				ApplyDamage({
 					victim = caster,
 					attacker = caster,
