8272412198bf6bdc901f9588aeca7c1e2b8043db
shimaosu <gagorka60@gmail.com>
2018-06-18 15:01:07 +0300

    balance(abilities): stegius - desolating touch now has boss limit

--- game/scripts/vscripts/heroes/hero_stegius/desolating_touch.lua
+++ game/scripts/vscripts/heroes/hero_stegius/desolating_touch.lua
@@ -2,14 +2,17 @@ function ReduceArmor(keys)
 	local caster = keys.caster
 	local unit = keys.target
 	local ability = keys.ability
-	if not unit:IsBoss() or RollPercentage(ability:GetAbilitySpecial("boss_armor_reduction_chance_pct")) then
-		local stacks = ability:GetAbilitySpecial("armor_per_hit")
-		ModifyStacks(ability, caster, unit, "modifier_stegius_desolating_touch_debuff", stacks, true)
-		unit:EmitSound("Item_Desolator.Target")
-		Timers:CreateTimer(ability:GetAbilitySpecial("duration"), function()
-			if IsValidEntity(caster) and IsValidEntity(unit) then
-				ModifyStacks(ability, caster, unit, "modifier_stegius_desolating_touch_debuff", -stacks)
-			end
-		end)
+	local stacks = ability:GetAbilitySpecial("armor_per_hit")
+	local modifier = unit:FindModifierByName("modifier_stegius_desolating_touch_debuff")
+	ModifyStacks(ability, caster, unit, "modifier_stegius_desolating_touch_debuff", stacks, true)
+	if unit:IsBoss() and modifier and modifier:GetStackCount() > ability:GetAbilitySpecial("limit") then
+		modifier:SetStackCount(ability:GetAbilitySpecial("limit"))
 	end
+	unit:EmitSound("Item_Desolator.Target")
+	Timers:CreateTimer(ability:GetAbilitySpecial("duration"), function()
+		if IsValidEntity(caster) and IsValidEntity(unit) then
+			ModifyStacks(ability, caster, unit, "modifier_stegius_desolating_touch_debuff", -stacks)
+		end
+	end)
 end
+
