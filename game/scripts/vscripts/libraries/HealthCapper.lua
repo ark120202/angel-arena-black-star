2c7a61914fdf4790cc2e595255015f2468bde597
Celireor <32054822+Celireor@users.noreply.github.com>
2018-06-21 00:40:55 +0800

    Prevent Saitama's HP from overflowing into negative values
    Also works for other heroes who need it

---
+++ game/scripts/vscripts/libraries/HealthCapper.lua
@@ -0,0 +1,30 @@
+if not HealthCapper then
+	HealthCapper = {}
+end
+
+function HealthCapper:Think()
+	
+	if not IsValidEntity(self) then
+		return
+	end
+	
+	local MaxHealth = self:GetMaxHealth()
+	if MaxHealth < 0 or MaxHealth > 2000000000 then
+		self:SetMaxHealth(2000000000)
+		self:SetHealth(2000000000)
+	end
+	
+	return 0.25
+end
+
+function HealthCapper:LogNPC(keys)
+	local npc = EntIndexToHScript(keys.entindex)
+	if npc:IsRealHero() and not npc.HealthCapper then	
+		npc.HealthCapper = true
+		Timers:CreateTimer(HealthCapper.Think, npc)
+  end
+end
+
+function HealthCapper:OnActivate() 
+    ListenToGameEvent('npc_spawned', Dynamic_Wrap(HealthCapper, 'LogNPC'), self)
+end
