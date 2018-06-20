097c5786c064ccec7d663e86134f022c871bf7a1
shimaosu <gagorka60@gmail.com>
2018-06-20 22:53:34 +0300

    style(abilities): desolating touch restyle

--- game/scripts/vscripts/heroes/hero_stegius/desolating_touch.lua
+++ game/scripts/vscripts/heroes/hero_stegius/desolating_touch.lua
@@ -5,8 +5,8 @@ function ReduceArmor(keys)
 	local stacks = ability:GetAbilitySpecial("armor_per_hit")
 	local modifier = unit:FindModifierByName("modifier_stegius_desolating_touch_debuff")
 	ModifyStacks(ability, caster, unit, "modifier_stegius_desolating_touch_debuff", stacks, true)
-	if unit:IsBoss() and modifier and modifier:GetStackCount() > ability:GetAbilitySpecial("limit") then
-		modifier:SetStackCount(ability:GetAbilitySpecial("limit"))
+	if unit:IsBoss() and modifier and modifier:GetStackCount() > ability:GetAbilitySpecial("boss_limit") then
+		modifier:SetStackCount(ability:GetAbilitySpecial("boss_limit"))
 	end
 	unit:EmitSound("Item_Desolator.Target")
 	Timers:CreateTimer(ability:GetAbilitySpecial("duration"), function()
@@ -15,4 +15,3 @@ function ReduceArmor(keys)
 		end
 	end)
 end
-
