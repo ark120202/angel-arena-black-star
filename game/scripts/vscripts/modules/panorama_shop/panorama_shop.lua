788518c6481c318b58289d9adb4c4d91d4bbc053
Celireor <32054822+Celireor@users.noreply.github.com>
2018-06-29 23:09:24 +0800

    Fixed minor dropped item flickering

--- game/scripts/vscripts/modules/panorama_shop/panorama_shop.lua
+++ game/scripts/vscripts/modules/panorama_shop/panorama_shop.lua
@@ -318,7 +318,7 @@ function PanoramaShop:PushItem(playerId, unit, itemName, bOnlyStash)
 		
 		if not isInShop then SetAllItemSlotsLocked(unit, false, true) end
 	end
-	Timers:CreateTimer(PanoramaShop.DropItemOnFailedPush, {item = item, playerId = playerId})
+	Timers:CreateTimer(0.04, PanoramaShop.DropItemOnFailedPush, {item = item, playerId = playerId})
 end
 
 function PanoramaShop.DropItemOnFailedPush (keys)
