aaeb4962439d227e8ed1f40db145b95cd02de300
Celireor <32054822+Celireor@users.noreply.github.com>
2018-06-29 11:25:22 +0800

    Update custom_hud.js

--- content/panorama/scripts/custom_hud.js
+++ content/panorama/scripts/custom_hud.js
@@ -243,9 +243,9 @@ function HookPanoramaPanels() {
 
 // On Death
 function OnDeath(data) {	
-	if(data.entindex_killed === SafeGetPlayerHeroEntityIndex(Game.GetLocalPlayerID()))
-		var killerOwner = Entities.GetPlayerOwnerID( data.entindex_attacker )
-		var attacker = 	Players.IsValidPlayerID( killerOwner ) ? SafeGetPlayerHeroEntityIndex(killerOwner) : data.entindex_attacker
+	if (data.entindex_killed === SafeGetPlayerHeroEntityIndex(Game.GetLocalPlayerID()))
+		var killerOwner = Entities.GetPlayerOwnerID(data.entindex_attacker)
+		var attacker = 	Players.IsValidPlayerID(killerOwner) ? SafeGetPlayerHeroEntityIndex(killerOwner) : data.entindex_attacker
 		FindDotaHudElement('KilledByHeroName').text = $.Localize(GetHeroName(attacker)).toUpperCase();
 }
 
@@ -356,7 +356,7 @@ function CreateHeroElements(id) {
 	});
 
 	AutoUpdatePanoramaHUD();
-	GameEvents.Subscribe('entity_killed', OnDeath );
+	GameEvents.Subscribe('entity_killed', OnDeath);
 	
 	GameEvents.Subscribe('create_custom_toast', CreateCustomToast);
 
