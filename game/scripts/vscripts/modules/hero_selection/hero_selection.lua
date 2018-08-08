bd7b6d4e53f47945338f9cc8350b4a0d56181a58
Celireor <32054822+Celireor@users.noreply.github.com>
2018-08-08 14:38:29 +0800

    Update hero_selection.lua

--- game/scripts/vscripts/modules/hero_selection/hero_selection.lua
+++ game/scripts/vscripts/modules/hero_selection/hero_selection.lua
@@ -76,8 +76,6 @@ function HeroSelection:PrepareTables()
 			local tabIndex = heroTable.base_hero and 2 or 1
 			local heroData = {
 				model = heroTable.base_hero or name,
-				custom_scene_camera = heroTable.SceneCamera,
-				custom_scene_image = heroTable.SceneImage,
 				attributes = HeroSelection:ExtractHeroStats(heroTable),
 				tabIndex = tabIndex
 			}
