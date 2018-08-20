a1798f5ec7fdb0cc139f11d6bd69e37e1d093690
Celireor <32054822+Celireor@users.noreply.github.com>
2018-08-20 09:39:50 +0800

    Create wisps.lua

---
+++ game/scripts/vscripts/heroes/hero_anakim/wisps.lua
@@ -0,0 +1,24 @@
+LinkLuaModifier("modifier_anakim_wisps", "heroes/hero_anakim/wisps.lua", LUA_MODIFIER_MOTION_NONE)
+require("kv")
+
+anakim_wisps = class({GetIntrinsicModifierName = function() return "modifier_anakim_wisps" end})
+
+modifier_anakim_wisps = class({
+	IsHidden = function() return true end,
+	IsPurgable = function() return false end,
+	DeclareFunctions = function() return {
+		MODIFIER_PROPERTY_PREATTACK_CRITICALSTRIKE,
+	} end
+})
+
+if IsServer() then
+	function modifier_anakim_wisps:OnCreated()
+		SetMaxLevel({ability = self:GetAbility()})
+	end
+
+	function modifier_anakim_wisps:GetModifierPreAttack_CriticalStrike()
+		if RandomInt(0, 100) <= self:GetAbility():GetSpecialValueFor("critical_chance_pct") then
+			return self:GetAbility():GetSpecialValueFor("critical_damage_pct")
+		end
+	end
+end
