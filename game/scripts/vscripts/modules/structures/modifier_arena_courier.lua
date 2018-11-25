88a832c0cc87dcdfb614cb7b8b56ced6b505b476
Arkady Pozdnyakov <ark120202@gmail.com>
2018-11-25 14:38:23 +0500

    chore: remove outdated max movespeed modifier property
    Refs #398.

--- game/scripts/vscripts/modules/structures/modifier_arena_courier.lua
+++ game/scripts/vscripts/modules/structures/modifier_arena_courier.lua
@@ -3,16 +3,7 @@ function modifier_arena_courier:IsHidden() return true end
 function modifier_arena_courier:IsPurgable() return false end
 function modifier_arena_courier:RemoveOnDeath() return false end
 
-function modifier_arena_courier:DeclareFunctions()
-	return {
-		MODIFIER_PROPERTY_MOVESPEED_MAX,
-		MODIFIER_PROPERTY_MOVESPEED_LIMIT,
-		MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE,
-	}
-end
-
-function modifier_arena_courier:GetModifierMoveSpeed_Max() return 800 end
-function modifier_arena_courier:GetModifierMoveSpeed_Limit() return 800 end
+function modifier_arena_courier:DeclareFunctions() return { MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE } end
 function modifier_arena_courier:GetModifierMoveSpeed_Absolute() return 800 end
 
 if IsServer() then
