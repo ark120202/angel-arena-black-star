f9d1c68197873871a72efdb09805a01c234b485a
Arkady Pozdnyakov <ark120202@gmail.com>
2018-11-25 21:08:43 +0500

    feat: change armor from agility formula
    Resolves #403.

--- content/panorama/scripts/custom_hud.js
+++ content/panorama/scripts/custom_hud.js
@@ -199,6 +199,8 @@ function HookPanoramaPanels() {
 			// https://dota2.gamepedia.com/Attack_speed#Attack_speed_representation
 			var attackSpeedTooltip = Entities.GetAttackSpeed(_unit) * 100 * (1.7 / attackRate);
 			DOTAHUDDamageArmorTooltip.SetDialogVariableInt('base_attack_speed', Math.round(attackSpeedTooltip));
+			if (custom_entity_value.IdealArmor != null)
+				DOTAHUDDamageArmorTooltip.SetDialogVariable('agility_armor', custom_entity_value.IdealArmor.toFixed(1));
 
 			if (custom_entity_value.AttributeStrengthGain != null)
 				DOTAHUDDamageArmorTooltip.SetDialogVariable('strength_per_level', custom_entity_value.AttributeStrengthGain.toFixed(1));
