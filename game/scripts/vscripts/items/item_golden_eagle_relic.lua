acfcf510f579da8de736391eca20f5ac3dcf3215
Celireor <32054822+Celireor@users.noreply.github.com>
2018-12-17 01:36:43 +0800

    Update item_golden_eagle_relic.lua

--- game/scripts/vscripts/items/item_golden_eagle_relic.lua
+++ game/scripts/vscripts/items/item_golden_eagle_relic.lua
@@ -8,11 +8,13 @@ item_golden_eagle_relic = class({
 if IsServer() then
 	function item_golden_eagle_relic:OnSpellStart()
 		local caster = self:GetCaster()
-		if caster:HasModifier("modifier_item_golden_eagle_relic_enabled") then
-			caster:RemoveModifierByName("modifier_item_golden_eagle_relic_enabled")
+		local enabledModifier = self.enabledModifier
+		if enabledModifier then
+			enabledModifier:Destroy()
 			caster:EmitSound("DOTA_Item.Armlet.DeActivate")
+			self.enabledModifier = nil
 		else
-			caster:AddNewModifier(caster, self, "modifier_item_golden_eagle_relic_enabled", nil)
+			self.enabledModifier = caster:AddNewModifier(caster, self, "modifier_item_golden_eagle_relic_enabled", nil)
 			caster:EmitSound("DOTA_Item.MaskOfMadness.Activate")
 		end
 	end
@@ -30,6 +32,14 @@ function modifier_item_golden_eagle_relic:DeclareFunctions()
 		MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT,
 		MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE
 	}
+end
+function modifier_item_golden_eagle_relic:OnDestroy()
+	local enabledModifier = self:GetAbility().enabledModifier
+	if enabledModifier then
+		enabledModifier:Destroy()
+		self:GetCaster():EmitSound("DOTA_Item.Armlet.DeActivate")
+		self:GetAbility().enabledModifier = nil
+	end
 end
 function modifier_item_golden_eagle_relic:GetModifierAttackSpeedBonus_Constant()
 	return self:GetAbility():GetSpecialValueFor("bonus_attackspeed")
@@ -51,15 +61,25 @@ function modifier_item_golden_eagle_relic_enabled:DeclareFunctions()
 		MODIFIER_EVENT_ON_ATTACK_LANDED
 	}
 end
+function modifier_item_golden_eagle_relic_enabled:OnCreated()
+	local ability = self:GetAbility()
+	self.active_bonus_attackspeed = ability:GetSpecialValueFor("active_bonus_attackspeed")
+	self.active_bonus_movespeed_pct = ability:GetSpecialValueFor("active_bonus_movespeed_pct")
+end
 function modifier_item_golden_eagle_relic_enabled:GetModifierAttackSpeedBonus_Constant()
-	return self:GetAbility():GetSpecialValueFor("active_bonus_attackspeed")
+	return self.active_bonus_attackspeed
 end
 function modifier_item_golden_eagle_relic_enabled:GetModifierMoveSpeedBonus_Percentage()
-	return self:GetAbility():GetSpecialValueFor("active_bonus_movespeed_pct")
+	return self.active_bonus_movespeed_pct
 end
 
 if IsServer() then
 	function modifier_item_golden_eagle_relic_enabled:OnCreated()
+		local ability = self:GetAbility()
+		self.active_gold_lost_per_sec_pct = ability:GetAbilitySpecial("active_gold_lost_per_sec_pct")
+		self.active_gold_lost_per_sec_min = ability:GetAbilitySpecial("active_gold_lost_per_sec_min")
+		self.active_gold_as_damage_pct = ability:GetSpecialValueFor("active_gold_as_damage_pct")
+		self.dmgType = ability:GetAbilityDamageType()
 		self.interval = 0.5
 		self:StartIntervalThink(self.interval)
 		self:OnIntervalThink()
@@ -72,24 +92,22 @@ if IsServer() then
 
 	function modifier_item_golden_eagle_relic_enabled:OnIntervalThink()
 		local parent = self:GetParent()
-		local ability = self:GetAbility()
-		local gold = Gold:GetGold(parent) * ability:GetAbilitySpecial("active_gold_lost_per_sec_pct") * 0.01 * self.interval
-		Gold:RemoveGold(parent, math.max(gold, ability:GetAbilitySpecial("active_gold_lost_per_sec_min")))
+		local gold = Gold:GetGold(parent) * self.active_gold_lost_per_sec_pct * 0.01 * self.interval
+		Gold:RemoveGold(parent, math.max(gold, self.active_gold_lost_per_sec_min))
 	end
 
 	function modifier_item_golden_eagle_relic_enabled:OnAttackLanded(keys)
 		local target = keys.target
 		local attacker = keys.attacker
 		if attacker == self:GetParent() then
-			local ability = self:GetAbility()
 			if not (target.IsBoss and target:IsBoss()) and not attacker:IsIllusion() then
-				local dmg = Gold:GetGold(attacker) * ability:GetSpecialValueFor("active_gold_as_damage_pct") * 0.01
+				local dmg = Gold:GetGold(attacker) * self.active_gold_as_damage_pct * 0.01
 				ApplyDamage({
 					victim = target,
 					attacker = attacker,
 					damage = dmg,
-					damage_type = ability:GetAbilityDamageType(),
-					ability = ability
+					damage_type = self.dmgType,
+					ability = self:GetAbility()
 				})
 				SendOverheadEventMessage(nil, OVERHEAD_ALERT_BONUS_SPELL_DAMAGE, target, dmg, nil)
 			end
