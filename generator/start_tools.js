896ec03341bff2572ec17ac3a906fffe589089e9
Arkady Pozdnyakov <ark120202@gmail.com>
2018-12-28 23:27:59 +0500

    feat(generator): use reverse-symlinks to install addon instad of rename

--- generator/start_tools.js
+++ generator/start_tools.js
@@ -19,27 +19,5 @@ try {
 	process.exit(1);
 }
 
-
-const game = path.join(settings.dota, 'game/dota_addons');
-const content = path.join(settings.dota, 'content/dota_addons');
-try {
-	fs.renameSync(path.join(game, settings.name), path.join(game, '___custom_game___'));
-	fs.renameSync(path.join(content, settings.name), path.join(content, '___custom_game___'));
-} catch (err) {
-	console.error('Make sure you have access to dota directory and you have executed install.js before');
-	console.error(err);
-	process.exit(1);
-}
-
 process.chdir(path.join(settings.dota, 'game/bin/win64'));
 child_process.spawn(path.join(settings.dota, 'game/bin/win64/dota2cfg.exe'), ['-addon', settings.name], {detached: true});
-
-fs.ensureDirSync(path.join(game, settings.name));
-fs.ensureDirSync(path.join(content, settings.name));
-
-setTimeout(() => {
-	fs.removeSync(path.join(game, settings.name));
-	fs.removeSync(path.join(content, settings.name));
-	fs.renameSync(path.join(game, '___custom_game___'), path.join(game, settings.name));
-	fs.renameSync(path.join(content, '___custom_game___'), path.join(content, settings.name));
-}, settings.delay || 1600);
