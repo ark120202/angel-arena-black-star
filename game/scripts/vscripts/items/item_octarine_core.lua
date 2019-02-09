bca9736557857ef5b7ffd476eb7f1b3ff3612685
Celireor <32054822+Celireor@users.noreply.github.com>
2019-02-09 13:06:12 +0800

    Update item_octarine_core.lua

--- game/scripts/vscripts/items/item_octarine_core.lua
+++ game/scripts/vscripts/items/item_octarine_core.lua
@@ -2,7 +2,7 @@ function OnDealDamage(keys)
 	local caster = keys.caster
 	local target = keys.unit
 	local ability = keys.ability
-	if ability:RollPRD(ability:GetLevelSpecialValueFor("bash_chance", ability:GetLevel() - 1)) then
+	if ability:RollPRD(ability:GetLevelSpecialValueFor("bash_chance", ability:GetLevel() - 1), "bash_chance_PRD") then
 		target:AddNewModifier(caster, ability, "modifier_stunned", {duration = ability:GetLevelSpecialValueFor("bash_duration", ability:GetLevel() - 1)})
 	end
 end
