c18a34750254bffd7fefabbae651b34d1c84c887
Celireor <32054822+Celireor@users.noreply.github.com>
2019-02-09 13:08:16 +0800

    Update item_refresher_core.lua

--- game/scripts/vscripts/items/item_refresher_core.lua
+++ game/scripts/vscripts/items/item_refresher_core.lua
@@ -14,7 +14,7 @@ function OnDealDamage(keys)
 	local caster = keys.caster
 	local target = keys.unit
 	local ability = keys.ability
-	if RollPercentage(ability:GetLevelSpecialValueFor("bash_chance", ability:GetLevel() - 1)) and not caster:HasModifier("modifier_item_refresher_core_bash_cooldown") then
+	if ability:RollPRD(ability:GetLevelSpecialValueFor("bash_chance", ability:GetLevel() - 1), "bash_chance_PRD") and not caster:HasModifier("modifier_item_refresher_core_bash_cooldown") then
 		target:AddNewModifier(caster, ability, "modifier_stunned", {duration = ability:GetLevelSpecialValueFor("bash_duration", ability:GetLevel() - 1)})
 		ability:ApplyDataDrivenModifier(caster, caster, "modifier_item_refresher_core_bash_cooldown", {})
 	end
