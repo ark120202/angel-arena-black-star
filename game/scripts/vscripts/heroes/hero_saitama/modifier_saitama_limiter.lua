348d1f7c50926207f3dd896093f960931a7d5332
Celireor <32054822+Celireor@users.noreply.github.com>
2019-06-12 10:26:27 +0800

    balance(saitama): remove attack speed limit (#484)

--- game/scripts/vscripts/heroes/hero_saitama/modifier_saitama_limiter.lua
+++ game/scripts/vscripts/heroes/hero_saitama/modifier_saitama_limiter.lua
@@ -5,7 +5,31 @@ modifier_saitama_limiter = class({
 })
 
 function modifier_saitama_limiter:DeclareFunctions()
-	return {MODIFIER_EVENT_ON_DEATH}
+	return {
+		MODIFIER_EVENT_ON_DEATH,
+		MODIFIER_PROPERTY_BASE_ATTACK_TIME_CONSTANT,
+		MODIFIER_PROPERTY_ATTACK_POINT_CONSTANT,
+	}
+end
+
+local FRACTION_700 = 1/7
+
+function modifier_saitama_limiter:OnCreated()
+	self.BaseBAT = self:GetCaster():GetBaseAttackTime()
+	self.BATMod = 1
+end
+
+function modifier_saitama_limiter:GetModifierBaseAttackTimeConstant()
+	local AS = self:GetCaster():GetIncreasedAttackSpeed()
+	if AS > 7 then
+		local BATMod = (7 / AS)
+		self.BATMod = BATMod
+		return self.BaseBAT * BATMod
+	end
+end
+
+function modifier_saitama_limiter:GetModifierAttackPointConstant()
+	return 0.75 * self.BATMod --0.75 is the base attack point
 end
 
 if IsServer() then
