37ddc9fc1c961e1506435fa14ab37c1be4a7899a
Arkady Pozdnyakov <ark120202@gmail.com>
2019-12-27 16:27:44 +0500

    Revert "balance: make couriers faster and invulnerable"
    This reverts commit 20e018b2c0ddb798869ec66b1a270ad651038b14.

--- game/scripts/vscripts/modules/structures/modifier_arena_courier.lua
+++ game/scripts/vscripts/modules/structures/modifier_arena_courier.lua
@@ -1,24 +1,24 @@
-modifier_arena_courier = {
-	IsHidden = function() return true end,
-	IsPurgable = function() return false end,
-	RemoveOnDeath = function() return false end,
+modifier_arena_courier = class({})
+function modifier_arena_courier:IsHidden() return true end
+function modifier_arena_courier:IsPurgable() return false end
+function modifier_arena_courier:RemoveOnDeath() return false end
 
-	GetModifierMoveSpeed_Absolute = function() return 2000 end,
-	GetFixedDayVision = function() return 150 end,
-	GetFixedNightVision = function() return 150 end,
-}
+function modifier_arena_courier:DeclareFunctions() return { MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE } end
+function modifier_arena_courier:GetModifierMoveSpeed_Absolute() return 800 end
 
-function modifier_arena_courier:CheckState()
-	return {
-		[MODIFIER_STATE_NO_HEALTH_BAR] = true,
-		[MODIFIER_STATE_INVULNERABLE] = true,
-	}
-end
+if IsServer() then
+	function modifier_arena_courier:OnCreated()
+		local courier = self:GetParent()
+		self:StartIntervalThink(60)
+		courier:SetBaseMaxHealth(COURIER_HEALTH_BASE)
+		courier:SetMaxHealth(COURIER_HEALTH_BASE)
+		courier:SetHealth(COURIER_HEALTH_BASE)
+	end
 
-function modifier_arena_courier:DeclareFunctions()
-	return {
-		MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE,
-		MODIFIER_PROPERTY_FIXED_DAY_VISION,
-		MODIFIER_PROPERTY_FIXED_NIGHT_VISION,
-	}
+	function modifier_arena_courier:OnIntervalThink()
+		local courier = self:GetParent()
+		courier:SetBaseMaxHealth(courier:GetBaseMaxHealth() + COURIER_HEALTH_GROWTH)
+		courier:SetMaxHealth(courier:GetMaxHealth() + COURIER_HEALTH_GROWTH)
+		courier:SetHealth(courier:GetHealth() + COURIER_HEALTH_GROWTH)
+	end
 end
